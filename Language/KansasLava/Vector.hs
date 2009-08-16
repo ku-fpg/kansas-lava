44bf08d428a72a5c636deed64eefec1a9f47a8f2
andygill <andygill@24856d36-1318-4051-b656-8aa62b98cd2c>
2009-08-16 22:03:20 +0000

    adding implementation of 1 and 2D Matrixes, for use with Lava. Later will become its own package
    git-svn-id: https://subversion.ittc.ku.edu/svn/fpg/tools/kansas-lava@117 24856d36-1318-4051-b656-8aa62b98cd2c

--- Language/KansasLava/Vector.hs
+++ Language/KansasLava/Vector.hs
@@ -3,145 +3,48 @@ module Language.KansasLava.Vector where
 
 import Language.Haskell.TH
 import Language.KansasLava.TH
+import Language.KansasLava.X
+import Data.Array as A hiding (indices)
+import Prelude as P
+import Control.Applicative
 
---data X1 a = X1 a
---data TwoOf a = TwoOf a a
+data	Vector i a = Vector (Array i a)
 
+instance (Show a, Ix i) => Show (Vector i a) where
+	show = show . toList
 
-data	Vector i a = Vector [a]
-	deriving Show
+(!) :: (Ix n) => Vector n a -> n -> a
+(!) (Vector xs) n = xs A.! n
 
-(!) :: (Enum n) => Vector n a -> n -> a
-(!) (Vector xs) n = xs !! fromEnum n
-
-instance Functor (Vector i) where
+instance (Ix i) => Functor (Vector i) where
 	fmap f (Vector xs) = Vector (fmap f xs)
 
-coords :: (Enum i, Bounded i) => Vector i i
-coords = Vector [minBound .. maxBound]
+toList :: (Ix i) => Vector i a -> [a]
+toList (Vector a) = elems a
 
-fromList :: Vector i a -> [a]
-fromList (Vector xs) = xs
-
-toList :: (Enum i, Bounded i) => [a] -> Vector i a
-toList xs = check (Vector xs) minBound maxBound
+fromList :: (Bounded i, Ix i) => [a] -> Vector i a
+fromList xs = check minBound maxBound
     where 
-	check :: (Enum i) => Vector i a -> i -> i -> Vector i a
-	check res low high = if fromEnum low == 0 && (fromEnum high == length xs - 1)
-			     then res
-			     else error "bad side of list for toList, in Vector"
-
---------------------------------
-
-data X1 = Zero deriving Eq
-instance Show X1 where	show = show . fromEnum 
-instance Enum X1 where	toEnum 0	= Zero
-			toEnum n	= error $ "trying to represent " ++ show n ++ " inside bounding type"
-			fromEnum Zero 	= 0
-instance Num X1 where	fromInteger = toEnum . fromIntegral
+	check low high | length (range (low,high)) == length xs
+		       = Vector $ listArray (low,high) xs
+		       | otherwise
+		       = error "bad length of fromList for Vector"
 
 
-toEnumV :: (Enum a) => Int -> Int -> Maybe a 				
-toEnumV n m | n == m    = Nothing
-	    | otherwise = Just (toEnum n)
+indices :: (Bounded i, Ix i) => [i]
+indices = range (minBound,maxBound)
 
-fromEnumV :: (Enum t) => Maybe t -> Int -> Int
-fromEnumV Nothing  s = s
-fromEnumV (Just v) _ = fromEnum v
+-- I find this useful
+coord :: (Bounded i, Ix i) => Vector i i
+coord = fromList indices
 
-fromEnumV' :: (SizedType u, Enum (Pred u)) => u -> Int
-fromEnumV' u = case demote u of
-		     Nothing -> elementCount u - 1
-		     Just v  -> fromEnum v
+zipWith :: (Bounded i, Ix i) => (a -> b -> c) -> Vector i a -> Vector i b -> Vector i c
+zipWith f (Vector a) (Vector b) = fromList (P.zipWith f (elems a) (elems b))
 
---------------------------------
+forAll :: (Bounded i, Ix i) => Vector i a -> (i -> a -> b) -> Vector i b
+forAll a f = Language.KansasLava.Vector.zipWith f coord a
 
-class SizedType s where
-	type Pred s
-	elementCount :: s -> Int
-	promote :: Maybe (Pred s) -> s
-	demote ::  s -> Maybe (Pred s)
+instance (Ix i, Bounded i) => Applicative (Vector i) where
+	pure a = fmap (const a) coord
+	(Vector a) <*> (Vector b) = fromList (P.zipWith (\ a b -> a b) (elems a) (elems b))
 
-data Void = Void
-
-instance SizedType Void		  where type Pred Void 		= Void
-					elementCount _ 		= 0
-					promote _ 		= error "promote"
-					demote _ 		= error "demote"
-{-
-instance SizedType X1 	  where type Pred X1 	= Void
-					elementCount _ 		= 1
-					promote Nothing 	= Zero
-					promote (Just Void)	= Zero
-					demote Zero 		= Nothing
-					
-instance SizedType TwoOf 	  where type Pred TwoOf 	= X1
-					elementCount _  	= 2
-					promote         	= One
-					demote  (One v) 	= v
-instance SizedType ThreeOf 	  where type Pred ThreeOf 	= TwoOf
-					elementCount _  	= 3
-					promote         	= Two
-					demote  (Two v) 	= v
-instance SizedType FourOf 	  where type Pred FourOf 	= ThreeOf
-					elementCount _  	= 4
-					promote         	= Three
-					demote  (Three v) 	= v
--}
-{-
-instance Num Zero where
-	fromInteger 0 = Zero
-	fromInteger other = error $ "other" ++ show other
-
-instance Show One where
-	show (One Nothing)  = "1"
-	show (One (Just s)) = show s
-
-instance Num One where
-	fromInteger 1 = One Nothing
-	fromInteger n = One (Just (fromInteger n))	
--- instance Num 
-
-class Vector n where
-	data V n :: * -> *
-	(!) :: V n a -> n -> a
-	toList :: V n a -> [a]
-	fromList :: [a] -> V n a
-
-instance Vector Zero where
-	data V Zero a = X1 a
-	(X1 a) ! 0 = a
-	toList (X1 a) = [a]
-	fromList [a] = X1 a
-
-instance Vector One where
-	data V One a = TwoOf a a
-	(TwoOf a0 a1) ! 0 = a0
-	(TwoOf a0 a1) ! 1 = a1
-	toList (TwoOf a0 a1) = [a0,a1]
-	fromList [a0,a1] = TwoOf a0 a1
-
--}
-
---instance Vect
-me = [d| f = show . fromEnum 
-     |]
-
-$(sizedTypeGenFor 16)
-
--- $(sizedTypeGen (mkName "TwoOf") (mkName "One") (mkName "X1") 1)
--- $(sizedTypeGen (mkName "ThreeOf") (mkName "Two") (mkName "TwoOf") 2)
-
--- $(me2 ''ThreeOf (mkName "Two") ''TwoOf)
--- $(me2 ''FourOf (mkName "Three") ''ThreeOf)
-
-
--- InstanceD [] (AppT (ConT ''Enum) (ConT t))  []]
-
-{-[InstanceD [] (AppT (ConT GHC.Enum.Enum) (ConT Language.KansasLava.Vector.TwoOf)) 
-[FunD toEnum [Clause [VarP n_0] (NormalB (AppE (ConE Language.KansasLava.Vector.One)
- (CondE (InfixE (Just (VarE n_0)) (VarE GHC.Classes.==) (Just (LitE (IntegerL 1)))) 
-(ConE Data.Maybe.Nothing) (AppE (ConE Data.Maybe.Just) (AppE (VarE toEnum) (VarE n_0)))))) []],
-FunD fromEnum [Clause [ConP Language.KansasLava.Vector.One [ConP Data.Maybe.Nothing []]] (NormalB (LitE (IntegerL 1))) [],Clause [ConP Language.KansasLava.Vector.One [ConP Data.Maybe.Just [VarP v_1]]] (NormalB (AppE (VarE fromEnum) (VarE v_1))) []]]]
-
--}
