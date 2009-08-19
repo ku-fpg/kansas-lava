2f6139404e6790f3a0466769cfe5a86165c87d7f
andygill <andygill@24856d36-1318-4051-b656-8aa62b98cd2c>
2009-08-19 16:21:09 +0000

    adding Time to delay
    git-svn-id: https://subversion.ittc.ku.edu/svn/fpg/tools/kansas-lava@126 24856d36-1318-4051-b656-8aa62b98cd2c

--- Language/KansasLava/Sequential.hs
+++ Language/KansasLava/Sequential.hs
@@ -10,19 +10,28 @@ import Language.KansasLava.Seq as Seq
 -- delay = error "delay!"
 
 -- Should this be a -> S a -> S a ??
-delay :: {- Signal System -> -} Signal a -> Signal a -> Signal a
-delay ~(Signal d def) ~(Signal rest w) 
+delay :: Signal Time -> Signal a -> Signal a -> Signal a
+delay ~(Signal tm tm_w) ~(Signal d def) ~(Signal rest w) 
         = Signal (Seq.head d :~ rest) 
         $ Port (Var "o")
         $ E
-        $ Entity (name "delay") [Var "o"] [(Var "init",def),(Var "i",w)]
+        $ Entity (name "delay") [Var "o"] [(Var "t",tm_w), (Var "init",def),(Var "i",w)]
 			[[ TyVar $ Var v | v <- ["o","init","i"]]]
 
 
-data System = System Integer Integer	-- global and local clocks
+data Time = Time Integer 		-- local clocks
+					-- 0 == reset signal
+					-- 1 == first cycle
+					-- 2 == second cycle
 
-clk :: Signal System -> Signal ()
-clk = undefined
-rst :: Signal System -> Signal Bool
-rst = undefined
+time :: Signal Time -> Signal Integer		-- simluation only
+time = fmap (\ (Time t) -> t)
+
+clock :: Signal Time
+clock = Signal (Seq.fromList $ map Just $ map Time [0..]) (Port (Var "o0") $ E $ Entity (Name "Lava" "clock") [] [] [[TyVar $ Var "o0",BaseTy T]])
+
+instance OpType Time    
+  where op _ nm = Name "Time" nm 
+	bitTypeOf _ = T
+	initVal = error "can not use Signal Time as an init value"
 
