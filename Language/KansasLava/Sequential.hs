3531c32388339af00aa943c9d7ff2619a1765342
andygill <andygill@24856d36-1318-4051-b656-8aa62b98cd2c>
2009-08-19 18:26:42 +0000

    Adding new clock and time support
    git-svn-id: https://subversion.ittc.ku.edu/svn/fpg/tools/kansas-lava/kansas-lava@127 24856d36-1318-4051-b656-8aa62b98cd2c

--- Language/KansasLava/Sequential.hs
+++ Language/KansasLava/Sequential.hs
@@ -4,7 +4,7 @@ import Language.KansasLava.Entity
 import Language.KansasLava.Signal
 import Language.KansasLava.Type
 import Language.KansasLava.Seq as Seq
-
+import Control.Applicative
 
 --delay :: (Sig sig) => sig a -> sig a -> sig a
 -- delay = error "delay!"
@@ -12,11 +12,21 @@ import Language.KansasLava.Seq as Seq
 -- Should this be a -> S a -> S a ??
 delay :: Signal Time -> Signal a -> Signal a -> Signal a
 delay ~(Signal tm tm_w) ~(Signal d def) ~(Signal rest w) 
-        = Signal (Seq.head d :~ rest) 
+        = Signal (shallowDelay tm d rest)
         $ Port (Var "o")
         $ E
-        $ Entity (name "delay") [Var "o"] [(Var "t",tm_w), (Var "init",def),(Var "i",w)]
+        $ Entity (Name "Lava" "delay") [Var "o"] [(Var "t",tm_w), (Var "init",def),(Var "i",w)]
 			[[ TyVar $ Var v | v <- ["o","init","i"]]]
+
+shallowDelay :: Seq Time -> Seq a -> Seq a -> Seq a
+shallowDelay sT sInit input =
+	pure (\ reset a b -> if reset then a else b)
+		<*> reset_delayed
+		<*> sInit
+		<*> input_delayed
+   where
+	input_delayed = Nothing :~ input
+	reset_delayed = Nothing :~ ((\ (Time t) -> t == -1) <$> sT)
 
 
 data Time = Time Integer 		-- local clocks
@@ -24,11 +34,28 @@ data Time = Time Integer 		-- local clocks
 					-- 1 == first cycle
 					-- 2 == second cycle
 
+instance Show Time where
+	show (Time t) = show t
+
+
 time :: Signal Time -> Signal Integer		-- simluation only
 time = fmap (\ (Time t) -> t)
 
 clock :: Signal Time
 clock = Signal (Seq.fromList $ map Just $ map Time [0..]) (Port (Var "o0") $ E $ Entity (Name "Lava" "clock") [] [] [[TyVar $ Var "o0",BaseTy T]])
+
+reset :: Signal Time 
+reset = Signal (pure (Time (-1))) (Port (Var "o0") $ E $ Entity (Name "Lava" "clock") [] [] [[TyVar $ Var "o0",BaseTy T]])
+
+switch :: Int -> Signal a -> Signal a -> Signal a
+switch n (Signal s1 _) (Signal s2 _) 
+  = Signal (pure (\ s1 s2 i -> if i < n then s1 else s2) 
+		<*> s1
+		<*> s2
+		<*> (Seq.fromList [ Just i | i <- [0..]])
+	   ) undefined
+	
+
 
 instance OpType Time    
   where op _ nm = Name "Time" nm 
