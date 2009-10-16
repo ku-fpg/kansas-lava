1241615bb1ecc40219beaae3d7a8b605d2472836
Garrin Kimmell <kimmell@ku.edu>
2009-10-16 14:14:20 -0500

    Checkpointing.

--- Language/KansasLava/CaseExtract.hs
+++ Language/KansasLava/CaseExtract.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE TemplateHaskell, ParallelListComp, UndecidableInstances #-}
-module Language.KansasLava.CaseExtract(lava,Ent(..)) where
+module Language.KansasLava.CaseExtract(lava,Ent(..),Signal(..)) where
 
 import Language.Haskell.TH
 import Language.Haskell.TH.Quote as Q
@@ -30,18 +30,15 @@ lava = QuasiQuoter parseExprExp parseExprPat
 convertExpr :: Exp -> Q Exp
 convertExpr (CaseE e alts) = do
     lcase (convertExpr e) alts
-  where
-        alts' :: [(String,[String],ExpQ)]
-        alts' = []
 
 convertExpr e = return e
 
--- lcase :: ExpQ -> [(String,[String],ExpQ)] -> ExpQ
+lcase :: ExpQ -> [Match] -> ExpQ
 lcase dis alts = [|
    let tagWidth = getTag $dis
        tagRange = (tagWidth - 1, 0)
        cons = getCons $dis
-       inputs = $(listE $ map (mkAlt [| cons |]) alts)
+       inputs = $(listE $ map (mkAlt' [| cons |]) alts)
    in Mux (Slice tagRange $dis) inputs
  |]
   where mkAlt cs (n,[],body) = body -- no pattern vars means no lookup/let binding
@@ -50,12 +47,28 @@ lcase dis alts = [|
              in  $(mkPats dis [| slices |] pvars body)
            |]
 
+        -- no pattern vars means no lookup/let binding
+        mkAlt' :: ExpQ -> Match -> ExpQ
+        mkAlt' cs (Match (ConP n []) (NormalB body) []) =
+          return body
+        mkAlt' cs (Match (ConP n pvars) (NormalB body) []) =
+          [| let Just slices = lookup $(litE (stringL (nameBase n)))  $cs
+             in  $(mkPats' dis [| slices |] pvars body)
+           |]
+        mkAlt' cs m = error $ "mkAlt: can't convert  " ++ show m
+
 mkPats dis slices pvars body
   = letE [valD (varP (mkName v)) (normalB [| Slice $dis ($slices !! s) |]) []
           |  v <- pvars
           |  s <- [0..]]
        body
 
+mkPats' :: ExpQ -> ExpQ -> [Pat] -> Exp -> ExpQ
+mkPats' dis slices pvars body
+  = letE [valD (return v) (normalB [| Slice  ($slices !! s) $dis |]) []
+          |  v <- pvars
+          |  s <- [0..]]
+       (return body)
 
 
 
@@ -68,10 +81,29 @@ data Ent = Mux Ent [Ent]
 
 instance Show Ent where
  show (Mux sel args) = "mux(s=> " ++ show sel ++ "," ++  (concat $ intersperse "," (map show args)) ++ ")"
- show (Slice (low,high) val) = show val ++  "(" ++ show high ++ " downto " ++ show low ++ ")"
+ show (Slice (high,low) val) = show val ++  "(" ++ show high ++ " downto " ++ show low ++ ")"
  show (Pad p) = p
  show (Lit x) = show x
 
+data Signal a = Sig Ent
+
+slice ran (Sig m) = Sig (Slice ran m)
+mux :: Signal a -> [Signal b] -> Signal b
+mux (Sig sel) args = Sig $ Mux sel [a | Sig a <- args]
+pad = Sig . Pad
+
+lit :: Int -> Signal Int
+lit = Sig . Lit
+
+
+
+
+instance Show (Signal a) where
+  show (Sig x) = show x
+
+instance CType a => CType (Signal a) where
+  getTag _ = getTag (undefined :: a)
+  getCons _ = getCons (undefined :: a)
 
 
 type ConsMap = [(String,[(Int,Int)])]
@@ -107,7 +139,7 @@ instance CType Ent where
 
 
 
-
+{-
 test = (lcase dise alts)
   where dis :: Maybe Int
         dis = Just 0
@@ -116,3 +148,4 @@ test = (lcase dise alts)
                 ("Just", ["v"],dise)]
 
 
+-}
