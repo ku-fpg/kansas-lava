6892b717c5c0b993cc0720b43ee91fb4fcebd4b7
Andy Gill <andygill@ku.edu>
2010-02-05 17:06:50 -0600

    Opps. I forgot to commit Utils, which is the bulk of the new work.

---
+++ Language/KansasLava/Utils.hs
@@ -0,0 +1,173 @@
+{-# LANGUAGE FlexibleContexts, UndecidableInstances, TypeFamilies, ParallelListComp, ScopedTypeVariables, FlexibleInstances, MultiParamTypeClasses  #-}
+
+module Language.KansasLava.Utils where
+	
+import Language.KansasLava.Entity as E
+import Language.KansasLava.Type
+import Language.KansasLava.Seq
+import Language.KansasLava.E
+import Language.KansasLava.Wire
+import Language.KansasLava.K
+import Language.KansasLava.Signal
+import Data.Sized.Matrix	as M
+
+import Control.Applicative
+import Data.Word
+import Data.Bits
+import Data.Ratio
+
+-----------------------------------------------------------------------------------------------
+
+instance Constant Bool where
+--  pureS v = liftS0 $ K (pureX v) $ D $ Lit $ fromWireRep v
+
+instance Constant Int where
+--  pureS v = liftS0 $ K (pureX v) $ D $ Lit $ fromIntegral v
+
+instance Constant Word32 where
+--  pureS v = liftS0 $ K (pureX v) $ D $ Lit $ fromIntegral v
+
+instance Constant Integer where
+--  pureS v = liftS0 $ K (pureX v) $ D $ Lit v
+
+high, low :: Signal Bool
+high = pureS True
+low  = pureS False
+
+true, false :: K Bool
+true = pureS True
+false = pureS False
+
+-----------------------------------------------------------------------------------------------
+and2 :: (SIGNAL sig) => sig Bool -> sig Bool -> sig Bool
+and2 = liftS2 $ \ (K a ae) (K b be) -> K (liftA2 (&&) a b) $ entity2 (Name "Bool" "and2") ae be
+
+or2 :: (SIGNAL sig) => sig Bool -> sig Bool -> sig Bool
+or2 = liftS2 $ \ (K a ae) (K b be) -> K (liftA2 (||) a b) $ entity2 (Name "Bool" "or2") ae be
+
+xor2 :: (SIGNAL sig) => sig Bool -> sig Bool -> sig Bool
+xor2 = liftS2 $ \ (K a ae) (K b be) -> K (liftA2 (/=) a b) $ entity2 (Name "Bool" "xor2") ae be
+
+bitNot :: (SIGNAL sig) => sig Bool -> sig Bool
+bitNot = liftS1 $ \ (K a ae) -> K (liftA (not) a) $ entity1 (Name "Bool" "not") ae
+
+testABit :: forall sig a . (Bits a, Wire a, SIGNAL sig) => sig a -> Int -> sig Bool
+testABit x y = liftS1 (\ (K a e) -> K (optX $ liftA (flip testBit y) (unX a :: Maybe a)) $ undefined) x
+{-
+	o0 $ entity1 (Name "Bits" "testABit") inputs [Var "o0"] (\ x' -> testBit x' y) x
+	where inputs   = take 2 defaultInputs
+-}
+-----------------------------------------------------------------------------------------------
+
+instance (Constant a, 	SIGNAL sig, Eq (sig a), Show (sig a), Num a, Wire a) => Num (sig a) where
+    s1 + s2 = fun2 "+" (+) s1 s2
+    s1 - s2 = fun2 "-" (-) s1 s2
+    s1 * s2 = fun2 "*" (*) s1 s2
+    negate s = fun1 "negate" (negate) s
+    abs s    = fun1 "abs"    (abs)    s
+    signum s = fun1 "signum" (signum) s
+    fromInteger n = pureS (fromInteger n)
+
+instance (Constant a, SIGNAL sig, Eq (sig Int), Show (sig Int), Eq (sig a), Show (sig a), Bits a, Wire a) 
+	=> Bits (sig a) where
+    s1 .&. s2 = fun2 ".&." (.&.) s1 s2
+    s1 .|. s2 = fun2 ".|." (.|.) s1 s2
+    s1 `xor` s2 = fun2 "xor" (xor) s1 s2
+    s1 `shift` n = fun2 "shift" (shift) s1 (fromIntegral n)
+    s1 `rotate` n = fun2 "rotate" (rotate) s1 (fromIntegral n)
+    complement s = fun1 "complement" (complement) s
+    bitSize s                       = baseTypeLength (bitTypeOf s)
+    isSigned s                      = baseTypeIsSigned (bitTypeOf s)
+
+instance (Constant a, SIGNAL sig, Eq (sig a), Show (sig a), Fractional a, Wire a) => Fractional (sig a) where
+    s1 / s2 = fun2 "/" (/) s1 s2
+    recip s1 = fun1 "/" (recip) s1 
+    fromRational r = fun2 "fromRational" (\ x y -> fromRational (x % y)) (pureS $ numerator r) (pureS $ denominator r)
+
+
+-----------------------------------------------------------------------------------------------
+-- And the utilties that get this done.
+
+fun0 :: forall a sig . (SIGNAL sig, Wire a) => String -> a -> sig a
+fun0 nm a = liftS0 $ K (optX $ Just $ a) $ entity0 (Name (wireName (error "fun1" :: a)) nm)
+
+fun1 :: forall a b sig . (SIGNAL sig, Wire a, Wire b) => String -> (a -> b) -> sig a -> sig b
+fun1 nm f = liftS1 $ \ (K a ae) -> K (optX $ liftA f (unX a)) $ entity1 (Name (wireName (error "fun1" :: b)) nm) ae
+
+fun2 :: forall a b c sig . (SIGNAL sig, Wire a, Wire b, Wire c) => String -> (a -> b -> c) -> sig a -> sig b -> sig c
+fun2 nm f = liftS2 $ \ (K a ae) (K b be) -> K (optX $ liftA2 f (unX a) (unX b)) 
+	  $ entity2 (Name (wireName (error "fun2" :: c)) nm) ae be
+
+entity0 :: forall o . (Wire o) => Name -> D o
+entity0 nm = D $ Port (Var "o0") $ E $
+ 	Entity nm [(Var "o0",oTy)]
+		  []
+		  []
+   where oTy = wireType (error "entity0" :: o)
+
+entity1 :: forall a o . (Wire a, Wire o) => Name -> D a -> D o
+entity1 nm (D w1) = D $ Port (Var "o0") $ E $
+ 	Entity nm [(Var "o0",oTy)]
+		  [(inp,ty,val) | inp <- map Var ["i0","i1"] 
+				| ty <- [aTy] 
+				| val <- [w1]
+		  ] []
+   where aTy = wireType (error "entity1" :: a)
+         oTy = wireType (error "entity1" :: o)
+
+entity2 :: forall a b o . (Wire a, Wire b, Wire o) => Name -> D a -> D b -> D o
+entity2 nm (D w1) (D w2) = D $ Port (Var "o0") $ E $
+ 	Entity nm [(Var "o0",oTy)]
+		  [(inp,ty,val) | inp <- map Var ["i0","i1"]
+				| ty <- [aTy,bTy] 
+				| val <- [w1,w2]
+		  ] []
+   where aTy = wireType (error "entity2" :: a)
+         bTy = wireType (error "entity2" :: b)
+         oTy = wireType (error "entity2" :: o)
+
+entityN :: forall a b o . (Wire a, Wire o) => Name -> [D a] -> D o
+entityN nm ds = D $ Port (Var "o0") $ E $
+ 	Entity nm [(Var "o0",oTy)]
+		  [(inp,ty,val) | inp <- map Var ["i" ++ show n | n <- [0..]]
+				| ty <- repeat aTy
+				| val <- [w | D w <- ds]
+		  ] []
+   where aTy = wireType (error "entity2" :: a)
+         oTy = wireType (error "entity2" :: o)
+
+
+
+-----------------------------------------------------------------------------------------------
+class (SIGNAL sig) => Pack sig a where
+ type Unpacked sig a
+ pack :: Unpacked sig a -> sig a
+ unpack :: sig a -> Unpacked sig a
+
+instance (Wire a, Wire b, SIGNAL sig) => Pack sig (a,b) where 
+	type Unpacked sig (a,b) = (sig a, sig b)
+	pack (a,b) = liftS2 (\ (K a ae) (K b be) -> K (a,b) (entity2 (Name "Lava" "pair") ae be))
+			    a b
+	unpack ab = ( liftS1 (\ (K (~(a,b)) abe) -> K a (entity1 (Name "Lava" "fst") abe)) ab
+		    , liftS1 (\ (K (~(a,b)) abe) -> K b (entity1 (Name "Lava" "snd") abe)) ab
+		    )
+
+instance (Wire a, SIGNAL sig, Num ix, Size ix) => Pack sig (Matrix ix a) where 
+	type Unpacked sig (Matrix ix a) = Matrix ix (sig a)
+	pack m = liftSL (\ ms -> let sh = M.fromList [ m | K m  _ <- ms ] 
+				     de = entityN (Name "Lava" "concat") [ d | K _ d <- ms ]
+				 in K sh de) (M.toList m) 
+	unpack s = forAll $ \ ix -> 
+			liftS1 (\ (K s d) -> K (s ! ix) undefined) s
+
+	
+pairSig :: (Wire a, Wire b, SIGNAL sig) => sig a -> sig b -> sig (a,b) 
+pairSig = liftS2 $ \ (K a ae) (K b be) -> K (a,b) (entity2 (Name "Lava" "pair") ae be)
+
+fstSig :: (Wire a, Wire b, SIGNAL sig) => sig (a,b) -> sig a
+fstSig = liftS1 $ \ (K (~(a,b)) abe) -> K a (entity1 (Name "Lava" "fst") abe)
+
+sndSig :: (Wire a, Wire b, SIGNAL sig) => sig (a,b) -> sig b
+sndSig = liftS1 $ \ (K (~(a,b)) abe) -> K b (entity1 (Name "Lava" "snd") abe)
+
+-----------------------------------------------------------------------------------------------
