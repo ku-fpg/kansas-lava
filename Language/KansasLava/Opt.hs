f4ab837d61f61b27db48f95c47a807818ab91be1
Andy Gill <andygill@ku.edu>
2010-03-07 00:56:53 -0600

    Basic 3 stage (Opt+Copy+DCE) Optimizer.

--- Language/KansasLava/Opt.hs
+++ Language/KansasLava/Opt.hs
@@ -7,7 +7,9 @@ import Language.KansasLava.Type
 import Language.KansasLava.Wire
 import Language.KansasLava.Entity
 import Language.KansasLava.Reify
+import Data.List
 
+import Debug.Trace
 -- A very simple optimizer.
 
 -- This returns an optimized version of the Entity, or fails to optimize.
@@ -15,31 +17,89 @@ optimizeEntity :: (Unique -> Entity BaseTy Unique) -> Entity BaseTy Unique -> Ma
 optimizeEntity env (Entity (Name "Lava" "fst") [(o0,tO)] [(i0,tI,Port o0' u)] []) =
 	case env u of
 	    Entity (Name "Lava" "pair") [(o0'',tI')] [(i1',t1,p1),(i2',t2,p2)] []
-	       | o0' == o0'' -> return $ replaceWith (i1',t1,p1)
+	       | o0' == o0'' -> return $ replaceWith env o0 (i1',t1,p1)
 	    _ -> Nothing
 optimizeEntity env (Entity (Name "Lava" "snd") [(o0,tO)] [(i0,tI,Port o0' u)] []) =
 	case env u of
 	    Entity (Name "Lava" "pair") [(o0'',tI')] [(i1',t1,p1),(i2',t2,p2)] []
-	       | o0' == o0'' -> return $ replaceWith (i2',t2,p2)
+	       | o0' == o0'' -> return $ replaceWith env o0 (i2',t2,p2)
 	    _ -> Nothing	
 optimizeEntity env _ = Nothing
 
-replaceWith (i,t,Pad v) = Entity (Name "Lava" "id") [(Var "o0",t)] [(Var "i0",t,Pad v)] []
-replaceWith (i,t,other) = Entity (Name "Lava" "id") [(Var "o0",t)] [(Var "i0",t,other)] []
+replaceWith env o (i,t,other) = Entity (Name "Lava" "id") [(o,t)] [(i,t,other)] []
 --replaceWith (i,t,x) = error $ "replaceWith " ++ show (i,t,x)
 
+----------------------------------------------------------------------
+
+data Opt a = Opt a Int
+
+instance Monad Opt where
+    return a = Opt a 0
+    (Opt a n) >>= k = case k a of
+	 		Opt r m -> Opt r (n + m)
+
+
+----------------------------------------------------------------------
+
+-- copy elimination
+copyElimReifiedCircuit :: ReifiedCircuit -> Opt ReifiedCircuit
+copyElimReifiedCircuit rCir = trace (show renamings) $ Opt rCir' (length renamings) 
+    where
+	env0 = theCircuit rCir
+	
+	rCir' = rCir 
+	      { theSinks = 
+		  [ ( v,t,
+		      case d of
+		        Port p u -> case lookup (u,p) renamings of
+				      Just other -> other
+				      Nothing    -> Port p u
+		    )
+		  | (v,t,d) <- theSinks rCir
+		  ]
+	      }
+
+	renamings = [ ((u,o),other)
+		    | (u,Entity (Name "Lava" "id") [(o,tO)] [(i,tI,other)] []) <- env0
+		    , tO == tI	-- should always be, anyway
+		    ]
+	
+
+dceReifiedCircuit :: ReifiedCircuit -> Opt ReifiedCircuit
+dceReifiedCircuit rCir = if optCount == 0
+			 then return rCir
+			 else Opt  rCir' optCount
+  where
+	optCount = length (theCircuit rCir) - length optCir
+
+	outFrees (_,_,Port _ u) = [u]
+	outFrees _             = []
+
+	allNames :: [Unique]
+	allNames = nub (concat
+		       [ case e of
+			  Entity nm _ outs _ -> concatMap outFrees outs
+		       | (u,e) <- theCircuit rCir
+		       ] ++ concatMap outFrees (theSinks rCir)
+		       )
+
+	optCir = [ (uq,orig)
+		 | (uq,orig) <- theCircuit rCir
+		 , uq `elem` allNames
+		 ]
+	rCir' = rCir { theCircuit = optCir }
 
 -- return Nothing *if* no optimization can be found.
-optimizeReifiedCircuit :: ReifiedCircuit -> Maybe ReifiedCircuit
+optimizeReifiedCircuit :: ReifiedCircuit -> Opt ReifiedCircuit
 optimizeReifiedCircuit rCir = if optCount == 0 
-			      then fail "Done Optimizing" 
-			      else return rCir'
+			      then return rCir	-- no changes
+			      else Opt rCir' optCount
   where
 	env0 = theCircuit rCir
 	env1 v = case lookup v env0 of
 		   Nothing -> error $ "oops, can not find " ++ show v
 		   Just e -> e
-
+		    
 	attemptOpt = [ optimizeEntity env1 e | (u,e) <- theCircuit rCir ]
 	optCount = length [ () | (Just _) <- attemptOpt ]
 
@@ -51,22 +111,19 @@ optimizeReifiedCircuit rCir = if optCount == 0
 
 	rCir' = rCir { theCircuit = optCir }
 
-{-
-
-		
-			{ theCircuit :: [(Unique,Entity BaseTy Unique)]
-		-- ^ This the main graph. There is no actual node for the source or sink.
-	, theSrcs    :: [(Var,BaseTy)]
-	, theSinks   :: [(Var,BaseTy,Driver Unique)]
-	-- , theTypes   :: TypeEnv
-	}	
--}
-
-optimizeReifiedCircuits :: ReifiedCircuit -> [ReifiedCircuit]
+optimizeReifiedCircuits :: ReifiedCircuit -> [(Int,ReifiedCircuit)]
 optimizeReifiedCircuits c = 
-	c : case optimizeReifiedCircuit c of
-	     Nothing -> []
-	     Just c' -> optimizeReifiedCircuits c'
+	case optimize c of
+	     Opt r 0 -> []
+	     Opt r n -> (n,r) : optimizeReifiedCircuits r
+   where
+	-- strange form, but does work, and allows easy reordering
+	optimize c = return c
+	    >>= optimizeReifiedCircuit
+	    >>= copyElimReifiedCircuit
+	    >>= dceReifiedCircuit
+
+
 
 showOptReifiedCircuit :: (Ports circuit) => [ReifyOptions] -> circuit -> IO String
 showOptReifiedCircuit opt c = do
@@ -81,5 +138,5 @@ showOptReifiedCircuit opt c = do
 		print rCir
 		loop (succ n) cs
 
-	rCir' <- loop 0 (optimizeReifiedCircuits rCir)
+	(_,rCir') <- loop 0 (optimizeReifiedCircuits rCir)
 	return $ show rCir'
