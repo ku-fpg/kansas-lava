07963c187edee9471cb834143d4bb3d467de4290
Andy Gill <andygill@ku.edu>
2010-04-16 22:57:00 -0500

    Adding a better StdLogicVector.

--- Language/KansasLava/StdLogicVector.hs
+++ Language/KansasLava/StdLogicVector.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ScopedTypeVariables, FlexibleContexts, TypeFamilies #-}
 
 module Language.KansasLava.StdLogicVector where
 
@@ -52,4 +52,27 @@ instance (Integral ix, Size ix) => Bits (StdLogicVector ix) where
 	            where 
 			ix' = fromIntegral ix - i
         testBit (StdLogicVector m) idx = m ! fromIntegral idx
+
+
+append :: (Size x, Size y, Size (ADD x y)) => StdLogicVector x -> StdLogicVector y -> StdLogicVector (ADD x y)
+append (StdLogicVector m1) (StdLogicVector m2) = (StdLogicVector $ M.matrix (M.toList m2 ++ M.toList m1))
+
+{-
+splice :: (Integral inp, Integral res, Size high, Size low, Size res, Size inp, res ~ ADD (SUB high low) X1) 
+       => high -> low -> StdLogicVector inp -> StdLogicVector res
+splice high low inp = StdLogicVector $ forAll $ \ ix -> inp' ! (fromIntegral ix)
+  where
+	StdLogicVector inp' = shiftR inp (size low)
+-}	
+
+splice :: forall inp res . (Integral inp, Integral res, Size res, Size inp)
+       => Int -> StdLogicVector inp -> StdLogicVector res
+splice low v = coerce $ shiftR v low
+
+-- either take lower bits, or append zeros to upper bits.
+coerce :: forall a b . (Size a, Size b) => StdLogicVector a -> StdLogicVector b
+coerce (StdLogicVector m) = StdLogicVector 
+			  $ M.matrix 
+			  $ take (size (error "witness" :: b))
+			  $ M.toList m ++ repeat False
 
