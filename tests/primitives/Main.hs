6e0d091a5dfae6a0dbe65d6dcf9739ed23921e07
Andrew Farmer <afarmer@ittc.ku.edu>
2010-04-27 01:56:04 -0500

    Fix the pad ordering problem (finally). Also, move testing framework into it's own file. Haskell now shells out to ssh to automatically run all testbenches in succession.

--- tests/primitives/Main.hs
+++ tests/primitives/Main.hs
@@ -19,30 +19,17 @@ import Data.Sized.Sampled as S
 import Data.Sized.Signed as S
 import Data.Sized.Unsigned as U
 
-import Debug.Trace
-
-import Language.KansasLava.Netlist
 import Language.KansasLava.Probes
-import Language.KansasLava.VHDL.Testbench
+import Language.KansasLava.Test.Probes
 
-import Language.Netlist.GenVHDL
-
-import System.Environment
 import System.Directory
-import System.Cmd
+import System.Environment
 import System.IO
 
 import qualified System.Posix.Env as Posix
 
-import Text.PrettyPrint(render)
-
 -- CONFIG --
 runTests = [] -- empty list builds every test
-
-numberOfCycles :: Int
-numberOfCycles = 100
-
-dumpDir = "examine/"
 -- END CONFIG --
 
 -- TESTS --
@@ -217,207 +204,9 @@ timesNeg0_75 (Comb a ea) = Comb (optX $ do a' <- unX a :: Maybe a
                                 (entity1 (Name "LDPC" "timesNeg0_75") ea)
 -- END HELPERS --
 
--- Everything below is to make the tests work. --
-halfAdder a b = (sum,carry)
-  where sum = xor2 a b
-        carry = and2 a b
-
-fullAdder a b cin = (sum,cout)
-  where (s1,c1) = probe "ha1" halfAdder a b
-        (sum,c2) = probe "ha2" halfAdder cin s1
-        cout = xor2 c1 c2
-
-probesTT probes = unlines
-    $ take numberOfCycles
-    $ mergeWith (\x y -> x ++ " | " ++ y)
-    $ map (\(_, (ProbeValue _ xs)) -> valsXStream xs) probes
-
-probesFor name plist = sortBy (\(n,_) (n2,_) -> compare n n2)
-                     $ filter (\(_, (ProbeValue nm _)) -> name `isPrefixOf` nm) plist
-
--- Run through the list of nodes that have probes on them and generate a list of
--- subcircuits that we can build testbenches for. This depends on the behavior of
--- List.intersect (elements come out in same order as they appear in first list).
-genRCs probes rc = go bfsOrder []
-    where bfsOrder = intersect (bfs (sinkNames rc) rc) probelist
-          probelist = map fst probes
-          go [] _ = []
-          go (n:ns) seen = [ (n, rc { theCircuit = trace ("newGraph: " ++ (unlines $ map show newGraph)) newGraph
-                                    , theSrcs = trace ("newPads: " ++ show newPads) newPads
-                                    , theSinks = trace ("newSinks: " ++ show newSinks) newSinks })
-                           | not $ prefix `elem` seen
-                           ] ++ (go ns (prefix:seen))
-              where
-                  newSinks = case lookup n (theCircuit rc) of
-                                Just (Entity _ outs _ _) -> [(Var $ "o" ++ show oname, ty, Port nm n)
-                                                            | (oname, (nm, ty)) <- zip [0..] outs ]
-                                Nothing -> error $ "mkSink failed on " ++ show n
-
-                  prefix = fst $ splitWith '_' $ last $ fromJust $ lookup n probes
-
-                  subtree = bfs' (\c -> (c == n) || (not (c `elem` probefamily))) [n] rc
-
-                  probefamily = [ node | (node, pnames) <- probes, pname <- pnames, prefix `isPrefixOf` pname ]
-
-                  subGraph = filter ((`elem` subtree) . fst) $ theCircuit rc
-
-                  leafNodes = [ (node, entity) | node <- probefamily, node /= n, Just entity <- [lookup node subGraph] ]
-
-                  newLeaves = [ (ch,Entity (Name "probe" ("i" ++ show i)) outs nins attrs)
-                              | (i, (ch, Entity _ outs ins attrs)) <- zip [0..] $ leafNodes
-                              , let nins = [ (Var $ "i" ++ show x, ty, Pad $ Var $ "i" ++ show i) | (x, (_,ty)) <- zip [0..] outs ]
-                              ]
-
-                  newGraph = newLeaves ++ (filter (not . (`elem` (map fst newLeaves)) . fst) subGraph)
-
-                  newPads = [ (v,ty) | (_, Entity _ _ ins _) <- newLeaves
-                                     , (_, ty, Pad v) <- ins ]
-
--- Seems like this should also exist in the Prelude
-splitWith :: Char -> String -> (String,String)
-splitWith c s = go s []
-    where go [] acc = (reverse acc,[])
-          go (i:inp) acc | i == c = (reverse acc,inp)
-                         | otherwise = go inp (i:acc)
- 
-bfs = bfs' (\_ -> True)
-bfs' fn nodes rc  = reverse $ go nodes []
-    where go [] acc = acc
-          go (n:ns) acc = go ns' acc' where acc' = n:acc
-                                            ns' = nub
-                                                $ filter (not . (`elem` acc'))
-                                                $ ns ++ (case fn n of
-                                                            True -> (children n rc)
-                                                            False -> [])
-
-sinkNames rc = map (\(_,_,Port _ n) -> n) $ theSinks rc
-children name rc = catMaybes $ filter isJust
-                   $ map (\(_,_,d) -> case d of
-                                        Port _ child -> Just child
-                                        _ -> Nothing) ins
-    where c = theCircuit rc
-          Entity _ _ ins _ = fromJust $ lookup name c
-
-probeValues name rc = [ pv | Just (Entity _ _ _ attrs) <- [lookup name $ theCircuit rc]
-                          , ("simValue", val) <- attrs
-                          , Just pv@(ProbeValue n v) <- [fromDynamic val]]
-
-testReify :: (Ports a) => String -> a -> IO ()
-testReify nm fn = do
-    putStrLn $ "Testing " ++ nm ++ " reify"
-    putStrLn $ "======="
-    debugCircuit [OptimizeReify] fn
-
-testCircuit' :: (Ports a, Probe a, Ports b) => [String] -> String -> a -> (a -> b) -> IO ()
-testCircuit' enabled name f apply
-    | null enabled || name `elem` enabled = do
-        let probed = probe name f
-
-        testReify name probed
-
-        plist <- probeCircuit $ apply probed
-
-        let trace = probesFor name plist
-
---        putStrLn $ "Truth table for " ++ name
---        putStrLn $ "======="
---        putStrLn $ probesTT trace
-
-        mkInputs name dumpDir numberOfCycles trace
-
-        putStrLn $ "Generating Testbench for " ++ name
-        putStrLn $ "======="
-        let ropts = [OptimizeReify]
-        mkTestbench ropts [] name probed   -- inc optimizations?
-
-        rc <- reifyCircuit ropts probed
-        applied_rc <- reifyCircuit ropts $ apply probed
-
-        let sinks = sinkNames rc
-            pvs = filter (not . null . snd) $ map ((\x -> (x, probeValues x rc)) . fst) $ theCircuit rc
-            subRCs = genRCs names rc
-            names = [(n,names) | (n,vs) <- pvs, let names = [ nm | ProbeValue nm _ <- vs ]]
-
-        mapM_ (mkTest ropts plist (dumpDir ++ name ++ "/") names) subRCs
-
-        return ()   
-    -- Hack to speed up the generation of our tests
-    | otherwise = return ()
-
-mkTest ropts plist base names (n,rc) = do
-    let name = fst $ splitWith '_' $ last $ fromJust $ lookup n names
-
-    mod <- netlistCircuit' [] name rc
-
-    let vhdl = render $ genVHDL mod
-
-    (inputs,outputs,sequentials) <- ports' ropts rc
-    waves <- genProbes' name rc
-
-    let trace = probesFor name plist
-
-    mkInputs name base numberOfCycles trace
-    mkTestbench' name base vhdl (inputs,outputs,sequentials) waves
-
-mkInputs name base count probes = do
-    let name_n = name ++ "_0"
-    let path = base ++ name ++ "/"
-
-    createDirectoryIfMissing True path
-
-    writeFile (path ++ "Makefile")
-        $ unlines
-        $ ["run : " ++ name ++ ".input " ++ name_n ++ ".info " ++ name ++ ".do"
-          ,"\t@echo \"Simulating...\""
-          ,"\t@vsim -c -do " ++ name ++ ".do"
-          ,"\t@echo \"10 lines from the info file...\""
-          ,"\t@tail " ++ name_n ++ ".info"
-          ,"\t@echo \"The same 10 lines from the input file...\""
-          ,"\t@tail " ++ name ++ ".input"
-          ,"\t@echo \"Ditto for the output file...\""
-          ,"\t@tail " ++ name ++ ".output"
-          ,"\t@./test.sh"
-          ]
-
-    writeFile (path ++ "test.sh")
-        $ unlines
-        $ ["#!/bin/bash"
-          ,"THEDIFF=`diff *.input *.output`"
-          ,""
-          ,"if [[ -z \"$THEDIFF\" ]]; then"
-          ,"    echo \"Input/Output Files Are The Same\""
-          ,"else"
-          ,"    echo \"Warning: Differences Below:\""
-          ,"    echo \"$THEDIFF\""
-          ,"fi"
-          ]
-    system $ "chmod +x " ++ path ++ "test.sh"
-
-    let bits = map (\(_, (ProbeValue _ xs)) -> bitsXStream xs) probes
-
-    writeFile (path ++ name ++ ".input")
-        $ unlines
-        $ take count
-        $ mergeWith (++)
-        $ bits
-
-    -- kind of messy I know
-    writeFile (path ++ name_n ++ ".info")
-        $ unlines
-        $ L.zipWith (\n l -> "(" ++ show n ++ ") " ++ l) [0..]
-        $ mergeWith (\x y -> x ++ " -> " ++ y)
-        $ L.zipWith (\bs vs -> L.zipWith (\v b -> v ++ "/" ++ b) (take count vs) (take count bs)) bits
-        $ map (\(_, (ProbeValue _ xs)) -> valsXStream xs) probes
-
--- surely this exists in the prelude?
-mergeWith :: (a -> a -> a) -> [[a]] -> [a]
-mergeWith fn probes = go probes []
-    where go (bs:ps) []  = go ps bs
-          go (bs:ps) acc = go ps $ L.zipWith fn acc bs
-          go []      acc = acc
-
-
+-- Everything below is for the old testing method.
 -- Lots of tests for primitives
+{-
 testSomeTruth:: (Testable a) => Int -> String -> a -> IO ()
 testSomeTruth n nm fn = do
     putStrLn $ "Testing " ++ nm ++ " function"
@@ -435,5 +224,5 @@ testSome nm tst f
     mkTestbench [OptimizeReify] [] nm tst   -- inc optimizations?
   -- Hack to speed up the generation of our tests
   | otherwise = putStrLn $ "Ignoring " ++ show nm
-
+-}
 		
