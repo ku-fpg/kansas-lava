4df71be66fee2728afc638b1209355fb3fdd7bfa
Andrew Farmer <afarmer@ittc.ku.edu>
2010-06-17 12:23:38 -0500

    Add netlist inst case for probe nodes.

--- Language/KansasLava/Netlist/Inst.hs
+++ Language/KansasLava/Netlist/Inst.hs
@@ -19,21 +19,21 @@ genInst :: Unique -> Entity BaseTy Unique -> [Decl]
 
 -- The replacements or aliases
 
-genInst i (Entity (Name "Lava" "pair") outputs inputs other) 
-	= genInst i (Entity (Name "Lava" "concat") outputs inputs other) 
-genInst i (Entity (Name "Lava" "triple") outputs inputs other) 
-	= genInst i (Entity (Name "Lava" "concat") outputs inputs other) 
+genInst i (Entity (Name "Lava" "pair") outputs inputs other)
+	= genInst i (Entity (Name "Lava" "concat") outputs inputs other)
+genInst i (Entity (Name "Lava" "triple") outputs inputs other)
+	= genInst i (Entity (Name "Lava" "concat") outputs inputs other)
 
 
-genInst i (Entity (Name "Lava" "fst") outputs inputs other) 
+genInst i (Entity (Name "Lava" "fst") outputs inputs other)
 	= genInst i (Entity (Name "Lava" "index") outputs (addNum 0 inputs) other)
-genInst i (Entity (Name "Lava" "snd") outputs inputs other) 
+genInst i (Entity (Name "Lava" "snd") outputs inputs other)
 	= genInst i (Entity (Name "Lava" "index") outputs (addNum 1 inputs) other)
-genInst i (Entity (Name "Lava" "fst3") outputs inputs other) 
+genInst i (Entity (Name "Lava" "fst3") outputs inputs other)
 	= genInst i (Entity (Name "Lava" "index") outputs (addNum 0 inputs) other)
-genInst i (Entity (Name "Lava" "snd3") outputs inputs other) 
+genInst i (Entity (Name "Lava" "snd3") outputs inputs other)
 	= genInst i (Entity (Name "Lava" "index") outputs (addNum 1 inputs) other)
-genInst i (Entity (Name "Lava" "thd3") outputs inputs other) 
+genInst i (Entity (Name "Lava" "thd3") outputs inputs other)
 	= genInst i (Entity (Name "Lava" "index") outputs (addNum 2 inputs) other)
 
 -- TMP aliases
@@ -42,8 +42,8 @@ genInst i (Entity n@(Name "X32" "+") outputs inputs dyn) =
 	genInst i (Entity (Name "Unsigned" "+") outputs inputs dyn)
 genInst i (Entity n@(Name "X32" "-") outputs inputs dyn) =
 	genInst i (Entity (Name "Unsigned" "-") outputs inputs dyn)
-genInst i (Entity n@(Name "Sampled" op) outputs inputs dyn) 
-      | op `elem` [".<.", ".>.", ".<=.", ".>=.", ".==."] 
+genInst i (Entity n@(Name "Sampled" op) outputs inputs dyn)
+      | op `elem` [".<.", ".>.", ".<=.", ".>=.", ".==."]
       = genInst i (Entity (Name "Signed" op) outputs inputs dyn)
 
 
@@ -57,7 +57,7 @@ genInst  i (Entity (Name "Lava" "concat") [(Var "o0",_)] inps _) =
                 [ toStdLogicExpr ty s | (Var _,ty, s) <- reverse inps]
 
 genInst i (Entity (Name "Lava" "index")
-		  [(Var "o0",outTy)] 
+		  [(Var "o0",outTy)]
 		  [(Var "i0",_, (Lit idx)),
 		   (Var "i1",ty,input)] _
 	   ) =
@@ -66,7 +66,12 @@ genInst i (Entity (Name "Lava" "index")
 		MatrixTy sz eleTy -> take sz $ repeat eleTy
 		TupleTy tys -> tys
 
--- 
+-- For Probes
+genInst i (Entity (Name "probe" _)
+         [(Var "o0",oty)]
+         [(Var "i0", _, input)] _) = [NetAssign (sigName "o0" i) (toTypedExpr oty input)]
+
+--
 genInst i e@(Entity (Name "Memory" "register") [(Var "o0",_)] inputs _) =
           [NetAssign input (toStdLogicExpr ty d) ]
   where output = sigName "o0" i
@@ -75,16 +80,16 @@ genInst i e@(Entity (Name "Memory" "register") [(Var "o0",_)] inputs _) =
 
 -- Muxes
 genInst i (Entity (Name _ "mux2") [(Var "o0",_)] [(Var i0,cTy,c),(Var i1 ,tTy,t),(Var i2,fTy,f)] _)
-	= [NetAssign (sigName "o0" i) 
+	= [NetAssign (sigName "o0" i)
                      (ExprCond cond
                       (toStdLogicExpr fTy t)
                       (toStdLogicExpr tTy f))]
   where cond = ExprBinary Equals (toTypedExpr cTy c) (ExprBit 1)
-	
+
 -- The following do not need any code in the inst segement
 
 genInst i (Entity nm outputs inputs _)
-	| nm `elem` [ Name "Memory" "BRAM" 
+	| nm `elem` [ Name "Memory" "BRAM"
 		    ]
 	= []
 
@@ -98,11 +103,11 @@ genInst i (Entity n@(Name _ _) [(Var "o0",oTy)] ins _)
 
 -- And the defaults
 
--- Right now, we *assume* that every external entity 
+-- Right now, we *assume* that every external entity
 -- has in and outs of type std_logic[_vector].
--- 
+--
 genInst i (Entity n@(Name mod_nm nm) outputs inputs _) =
-	trace (show ("mkInst",n,[ t | (_,t) <- outputs ],[ t | (_,t,_) <- inputs ])) $ 
+	trace (show ("mkInst",n,[ t | (_,t) <- outputs ],[ t | (_,t,_) <- inputs ])) $
           [ InstDecl (mod_nm ++ "_" ++ cleanupName nm) ("inst" ++ show i)
   		[ ("width_size",ExprNum $ fromIntegral $ head [ baseTypeLength ty | (_,ty) <- outputs ])
 			| mod_nms <- ["Sampled"]	-- hack
@@ -118,7 +123,7 @@ genInst i (Entity n@(Name mod_nm nm) outputs inputs _) =
 
 genInst i tab@(Table (Var vout,tyout) (vin,tyin,d) mp) =
 	[ NetAssign (sigName vout i)
-		(ExprCase (toStdLogicExpr tyin d) 
+		(ExprCase (toStdLogicExpr tyin d)
 			[ ([toStdLogicExpr tyin ix],toStdLogicExpr tyout val)
 			| (ix,_,val,_) <- mp
 			]
@@ -133,35 +138,35 @@ genInst i tab@(Table (Var vout,tyout) (vin,tyin,d) mp) =
 data NetlistOperation = NetlistOp Int (BaseTy -> [(BaseTy,Driver Unique)] -> Expr)
 
 mkSpecialUnary
-	:: (BaseTy -> Expr -> Expr) 
+	:: (BaseTy -> Expr -> Expr)
 	-> (BaseTy -> Driver Unique -> Expr)
-	-> [String] 
-	-> [(String, UnaryOp)] 
+	-> [String]
+	-> [(String, UnaryOp)]
 	-> [(Name, NetlistOperation)]
 mkSpecialUnary coerceR coerceF mtys ops =
        [( Name moduleName lavaName
-	, NetlistOp 1 $ \ fTy [(ity,i)] -> 
-		coerceR fTy (ExprUnary netListOp 
+	, NetlistOp 1 $ \ fTy [(ity,i)] ->
+		coerceR fTy (ExprUnary netListOp
 					(coerceF ity i))
-	
+
 	)
          | moduleName <- mtys
          , (lavaName,netListOp) <- ops
          ]
 
-mkSpecialBinary 
-	:: (BaseTy -> Expr -> Expr) 
+mkSpecialBinary
+	:: (BaseTy -> Expr -> Expr)
 	-> (BaseTy -> Driver Unique -> Expr)
-	-> [String] 
-	-> [(String, BinaryOp)] 
+	-> [String]
+	-> [(String, BinaryOp)]
 	-> [(Name, NetlistOperation)]
 mkSpecialBinary coerceR coerceF mtys ops =
        [( Name moduleName lavaName
-	, NetlistOp 2 $ \ fTy [(lty,l),(rty,r)] -> 
-		coerceR fTy (ExprBinary netListOp 
-					(coerceF lty l) 
-					(coerceF rty r))	
-	
+	, NetlistOp 2 $ \ fTy [(lty,l),(rty,r)] ->
+		coerceR fTy (ExprBinary netListOp
+					(coerceF lty l)
+					(coerceF rty r))
+
 	)
          | moduleName <- mtys
          , (lavaName,netListOp) <- ops
