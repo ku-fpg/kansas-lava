89d9196ec60fa8f7407baa3af0e1319922f8f134
Andy Gill <andygill@ku.edu>
2010-09-04 21:26:47 -0500

    Adding an internals module. This should export the advanced API for Kansas Lava.

---
+++ Language/KansasLava/Internals.hs
@@ -0,0 +1,8 @@
+-- | This module is the external API for the internals of Kansas Lava, if
+--   someone wants to see them.
+
+module Language.KansasLava.Internals 
+	( module Language.KansasLava.Types 
+	) where
+
+import Language.KansasLava.Types 
