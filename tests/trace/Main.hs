ccd2972e19eff55190531c9652b2be20f9ea193c
Andy Gill <andygill@ku.edu>
2010-09-06 12:57:21 -0500

    Adding some tests, not the correct place.

--- tests/trace/Main.hs
+++ tests/trace/Main.hs
@@ -7,6 +7,8 @@ import Data.Sized.Unsigned
 import Data.Default
 import Data.List
 
+import Data.Word
+
 -- TODO: Add Ports/InPorts instance for Signal? Otherwise probe below won't work
 -- halfAdder :: (Signal sig) => sig Bool -> sig Bool -> (sig Bool, sig Bool)
 halfAdder :: Seq Bool -> Seq Bool -> (Seq Bool, Seq Bool)
@@ -77,3 +79,37 @@ main = do
 
     t4 <- mkTrace limit muxt
     mkTarball "test/mux2" 100 muxt
+
+
+-- Andy's tests
+-- Can be moved or removed
+
+    do -- hack to get tabbed do
+
+
+	-- The lavaId is because we can not annotate funMap
+	let thunk5 = Thunk (lavaId . funMap (\ x -> return (x + 1)) . lavaId :: Seq Word8 -> Seq Word8)
+		       (\ f -> f (toSeq [1..100]))
+
+	print $ runT thunk5
+	trace5 <- mkTrace (return 200) thunk5
+	print $ trace5
+
+	let thunk6 = Thunk (pipeToMemory :: Env () -> Env () -> Seq (Pipe Word8 Int) -> Seq Word8 -> Seq Int)
+	       $ \ cir -> cir shallowEnv 
+			      shallowEnv 
+			      (toSeq $ [Nothing,Nothing,Nothing,Nothing,Nothing]
+				    ++ [ return (0,100)
+				       , return (1,101)
+				       , return (2,102)
+				       , return (3,103)
+				       , return (4,104)
+			       	       ]
+			 	    ++ cycle [Nothing])
+			      (toSeq $ cycle [0..2])
+
+	print $ runT thunk6
+	trace6 <- mkTrace (return 20) thunk6
+        print $ trace6
+
+    return ()
