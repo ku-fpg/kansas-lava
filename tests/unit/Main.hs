2327937838ddfb2f2f1d3764d5022f8f41c77f9c
Andy Gill <andygill@ku.edu>
2010-09-27 23:54:41 -0500

    Improving and adding tests.

--- tests/unit/Main.hs
+++ tests/unit/Main.hs
@@ -1,10 +1,12 @@
-{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables, RankNTypes #-}
 module Main where
 	
 import Language.KansasLava
 import Language.KansasLava.Testing.Thunk
 import Data.Sized.Ix
 import Data.Sized.Unsigned
+import Data.Sized.Signed
+import Data.Sized.Sampled
 import Data.Default
 import Data.List ( sortBy )
 import Data.Ord ( comparing )
@@ -17,7 +19,25 @@ main = do
 
 	testMux test
 
-	testBinOpNum test "U4" u4s0 u4s1
+	testBounded test "U1" (witness :: U1)
+	testBounded test "U2" (witness :: U2)
+	testBounded test "U3" (witness :: U3)
+	testBounded test "U4" (witness :: U4)
+	testBounded test "U5" (witness :: U5)
+	testBounded test "U6" (witness :: U6)
+	testBounded test "U7" (witness :: U7)
+	testBounded test "U8" (witness :: U8)
+
+	-- no S1
+	testBounded test "S2" (witness :: S2)
+	testBounded test "S3" (witness :: S3)
+	testBounded test "S4" (witness :: S4)
+	testBounded test "S5" (witness :: S5)
+	testBounded test "S6" (witness :: S6)
+	testBounded test "S7" (witness :: S7)
+	testBounded test "S8" (witness :: S8)
+
+	testValues test "Sampled/8x8" ([0..8] :: [Sampled X8 X8])
 
 -------------------------------------------------------------------------------------------------
 testMux :: TestSeq -> IO ()	
@@ -35,9 +55,10 @@ testMux (TestSeq test) = do
 	test "mux2/U4" 100 thu res
 
 -------------------------------------------------------------------------------------------------
+-- This only tests at the *value* level, and ignores testing unknowns.
 
-testBinOp :: (Rep a, Show a, Eq a) => TestSeq -> String -> (a -> a -> a) -> (Comb a -> Comb a -> Comb a) -> [a] -> [a] -> IO ()	
-testBinOp (TestSeq test) nm op lavaOp us0 us1 = do
+testBinOp :: (Rep a, Show a, Eq a) => TestSeq -> Int -> String -> (a -> a -> a) -> (Comb a -> Comb a -> Comb a) -> [a] -> [a] -> IO ()	
+testBinOp (TestSeq test) count nm op lavaOp us0 us1 = do
 	let thu = Thunk (liftS2 lavaOp)
 		        (\ f -> f (toSeq us0) (toSeq us1)
 			)
@@ -45,15 +66,32 @@ testBinOp (TestSeq test) nm op lavaOp us0 us1 = do
 				  us0
 				  us1
 			)
-	test nm 100 thu res
+	test nm count thu res
 
-testBinOpNum :: (Num a, Rep a) => TestSeq -> String -> [a] -> [a] -> IO ()
-testBinOpNum test tyName s0 s1 = 
+testBinOpNum :: (Num a, Rep a) => TestSeq -> Int -> String -> [a] -> [a] -> IO ()
+testBinOpNum test count tyName s0 s1 = 
 	sequence_
-	  [ testBinOp test (name ++ "/" ++ tyName)  (+) (+) s0 s1
+	  [ testBinOp test count (name ++ "/" ++ tyName)  (+) (+) s0 s1
           | (name,op,lavaOp) <- 
 		[ ("add",(+),(+))
 		, ("sub",(+),(+))
 		, ("mul",(+),(+))
 		]
 	  ]
+
+testValues :: forall w . (Rep w, Num w) => TestSeq -> String -> [w] -> IO ()
+testValues test tyName ws = do
+	let s1 :: [w]
+	    s2 :: [w]
+	    (s1,s2) = unzip
+		    [ (s1,s2) 
+		    | s1 <- ws
+		    , s2 <- ws
+		    ]
+	testBinOpNum test (length ws * length ws) tyName s1 s2
+
+testBounded :: forall w . (Rep w, Num w, Bounded w, Enum w) => TestSeq -> String -> w -> IO ()
+testBounded test tyName w = testValues test tyName ([minBound..maxBound] :: [w])
+
+
+
