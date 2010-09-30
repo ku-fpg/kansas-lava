f41dea5339d900542537160bd24fd10958a7643c
Andy Gill <andygill@ku.edu>
2010-09-29 22:21:23 -0500

    Refactor to use class higherarchy better.

--- tests/unit/Main.hs
+++ tests/unit/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE ScopedTypeVariables, RankNTypes, TypeFamilies, FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables, RankNTypes, TypeFamilies, FlexibleContexts, ExistentialQuantification #-}
 module Main where
 	
 import Language.KansasLava
@@ -12,6 +12,7 @@ import Data.List ( sortBy )
 import Data.Ord ( comparing )
 import Data.Maybe as Maybe
 import Control.Applicative
+import Data.Bits
 
 import Utils
 
@@ -24,37 +25,49 @@ main = do
 					   Nothing -> genToList
 					   Just i -> take i . genToRandom)
 
+	-- Just the Eq Stuff
+	let t str arb = testOpsEq test str arb
 
-	testOps test "U1" (arbitrary :: Gen U1)
-	testOps test "U2" (arbitrary :: Gen U2)
-	testOps test "U3" (arbitrary :: Gen U3)
-	testOps test "U4" (arbitrary :: Gen U4)
-	testOps test "U5" (arbitrary :: Gen U5)
-	testOps test "U6" (arbitrary :: Gen U6)
-	testOps test "U7" (arbitrary :: Gen U7)
-	testOps test "U8" (arbitrary :: Gen U8)
-	testOps test "U32" (arbitrary :: Gen U32)
-	testOps test "U64" (arbitrary :: Gen (Unsigned X64))
+	-- Just the Ord Stuff
+	let t str arb = testOpsOrd test str arb
+
+	t "Bool" (arbitrary :: Gen Bool)
+
+	-- Just the Num Stuff
+	let t str arb = testOpsNum test str arb
+
+	t "Sampled/8x8" (arbitrary :: Gen (Sampled X8 X8))
+	t "Sampled/4x2" (arbitrary :: Gen (Sampled X4 X2))
+	t "Sampled/2x2" (arbitrary :: Gen (Sampled X2 X2))
+	t "Sampled/2x1" (arbitrary :: Gen (Sampled X2 X1))
+	t "Sampled/1x2" (arbitrary :: Gen (Sampled X1 X2))
+	t "Sampled/1x4" (arbitrary :: Gen (Sampled X1 X4))
+	t "Sampled/8x10"(arbitrary :: Gen (Sampled X8 X10))
+
+	-- Just the Bits Stuff
+	let t str arb = testOpsBits test str arb
+
+	t "U1" (arbitrary :: Gen U1)
+	t "U2" (arbitrary :: Gen U2)
+	t "U3" (arbitrary :: Gen U3)
+	t "U4" (arbitrary :: Gen U4)
+	t "U5" (arbitrary :: Gen U5)
+	t "U6" (arbitrary :: Gen U6)
+	t "U7" (arbitrary :: Gen U7)
+	t "U8" (arbitrary :: Gen U8)
+	t "U32" (arbitrary :: Gen U32)
+	t "U64" (arbitrary :: Gen (Unsigned X64))
 
 	-- no S1
-	testOps test "S2" (arbitrary :: Gen S2)
-	testOps test "S3" (arbitrary :: Gen S3)
-	testOps test "S4" (arbitrary :: Gen S4)
-	testOps test "S5" (arbitrary :: Gen S5)
-	testOps test "S6" (arbitrary :: Gen S6)
-	testOps test "S7" (arbitrary :: Gen S7)
-	testOps test "S8" (arbitrary :: Gen S8)
-	testOps test "S32" (arbitrary :: Gen S32)
-	testOps test "S64" (arbitrary :: Gen (Signed X64))
-	
-	testOps test "Sampled/8x8" (arbitrary :: Gen (Sampled X8 X8))
-	testOps test "Sampled/4x2" (arbitrary :: Gen (Sampled X4 X2))		
-	testOps test "Sampled/2x2" (arbitrary :: Gen (Sampled X2 X2))	
-	testOps test "Sampled/2x1" (arbitrary :: Gen (Sampled X2 X1))		
-	testOps test "Sampled/1x2" (arbitrary :: Gen (Sampled X1 X2))		
-	testOps test "Sampled/1x4" (arbitrary :: Gen (Sampled X1 X4))		
-	testOps test "Sampled/8x10"(arbitrary :: Gen (Sampled X8 X10))
-
+	t "S2" (arbitrary :: Gen S2)
+	t "S3" (arbitrary :: Gen S3)
+	t "S4" (arbitrary :: Gen S4)
+	t "S5" (arbitrary :: Gen S5)
+	t "S6" (arbitrary :: Gen S6)
+	t "S7" (arbitrary :: Gen S7)
+	t "S8" (arbitrary :: Gen S8)
+	t "S32" (arbitrary :: Gen S32)
+	t "S64" (arbitrary :: Gen (Signed X64))
 
 allValues :: forall w . (Rep w) => [w]
 allValues = xs
@@ -99,7 +112,7 @@ testUniOp (TestSeq test toList) nm op lavaOp gen = do
 			)
 	test nm (length us0) thu res
 
-testBinOp :: (Rep a, Show a, Eq a, Rep b, Show b, Eq b) => TestSeq -> String -> (a -> a -> b) -> (Comb a -> Comb a -> Comb b) -> Gen (a,a) -> IO ()	
+testBinOp :: (Rep a, Show a, Eq c, Rep b, Show b, Eq b, Rep c, Show c) => TestSeq -> String -> (a -> b -> c) -> (Comb a -> Comb b -> Comb c) -> Gen (a,b) -> IO ()	
 testBinOp (TestSeq test toList) nm op lavaOp gen = do
 	let (us0,us1) = unzip $ toList gen
 	let thu = Thunk (liftS2 lavaOp)
@@ -111,10 +124,54 @@ testBinOp (TestSeq test toList) nm op lavaOp gen = do
 			)
 	test nm (length (zip us0 us1)) thu res
 
-testUniOpNum :: (Num a, Rep a) => TestSeq -> String -> Gen a -> IO ()
-testUniOpNum test tyName s0 = 
+testTriOp :: (Rep a, Show a, Eq c, Rep b, Show b, Eq b, Rep c, Show c, Rep d, Show d, Eq d) => TestSeq -> String -> (a -> b -> c -> d) -> (Comb a -> Comb b -> Comb c -> Comb d) -> Gen (a,b,c) -> IO ()	
+testTriOp (TestSeq test toList) nm op lavaOp gen = do
+	let (us0,us1,us2) = unzip3 $ toList gen
+	let thu = Thunk (liftS3 lavaOp)
+		        (\ f -> f (toSeq us0) (toSeq us1) (toSeq us2)
+			)
+ 	    res = toSeq (Prelude.zipWith3 op
+				  us0
+				  us1
+				  us2
+			)
+	test nm (length (zip us0 us1)) thu res
+
+------------------------------------------------------------------------------------------------
+
+testOpsEq :: (Rep w, Eq w, Show w) => TestSeq -> String -> Gen w -> IO ()
+testOpsEq test tyName ws = do
+	let ws2 = pair ws
+
 	sequence_
-	  [ testUniOp test (name ++ "/" ++ tyName) op lavaOp s0
+	  [ testTriOp test (name ++ "/" ++ tyName) op lavaOp 
+			(pure (\ c (a,b) -> (c,a,b)) 
+					  <*> arbitrary
+					  <*> ws2)
+          | (name,op,lavaOp) <- 
+		[ ("mux",\ c a b -> if c then a else b,\ c a b -> mux2 c (a,b))
+		]
+	  ]
+
+------------------------------------------------------------------------------------------------
+
+
+testOpsOrd :: (Rep w, Ord w, Show w) => TestSeq -> String -> Gen w -> IO ()
+testOpsOrd test tyName ws = do
+	testOpsEq test tyName ws
+
+------------------------------------------------------------------------------------------------
+
+
+testOpsNum :: forall w . 
+	(Ord w, Rep w, Num w) => TestSeq -> String -> Gen w -> IO ()
+testOpsNum test tyName ws = do
+	testOpsOrd test tyName ws
+
+	let ws2 = pair ws
+
+	sequence_
+	  [ testUniOp test (name ++ "/" ++ tyName) op lavaOp ws
           | (name,op,lavaOp) <- 
 		[ ("negate",negate,negate)
 		, ("abs",abs,abs)
@@ -122,18 +179,8 @@ testUniOpNum test tyName s0 =
 		]
 	  ]
 
-testBinOpNum :: 
-	(Size (ADD (WIDTH a) (WIDTH a)),
-         Enum (ADD (WIDTH a) (WIDTH a)),
-	Ord a,Num a, Rep a) => TestSeq -> String -> Gen (a,a) -> IO ()
-testBinOpNum test tyName gen = do
-
-	testMux test ("mux/" ++ tyName) (pure (\ c (a,b) -> (c,a,b)) 
-					  <*> arbitrary
-					  <*> gen)
-
 	sequence_
-	  [ testBinOp test (name ++ "/" ++ tyName)  op lavaOp gen
+	  [ testBinOp test (name ++ "/" ++ tyName)  op lavaOp ws2
           | (name,op,lavaOp) <- 
 		[ ("add",(+),(+))
 		, ("sub",(-),(-))
@@ -143,7 +190,7 @@ testBinOpNum test tyName gen = do
 		]
 	  ]
 	sequence_
-	  [ testBinOp test (name ++ "/" ++ tyName)  op lavaOp gen
+	  [ testBinOp test (name ++ "/" ++ tyName)  op lavaOp ws2
           | (name,op,lavaOp) <- 
 		[ ("==",(==),(.==.))
 --		, ("/=",(/=),(./=.))
@@ -154,10 +201,25 @@ testBinOpNum test tyName gen = do
 		]
 	  ]
 
-testOps :: forall w . 
-	(Size (ADD (WIDTH w) (WIDTH w)),
-         Enum (ADD (WIDTH w) (WIDTH w)), 
-	 Ord w, Rep w, Num w) => TestSeq -> String -> Gen w -> IO ()
-testOps test tyName ws = do
-	testUniOpNum test tyName ws
-	testBinOpNum test tyName (pure (,) <*> ws <*> ws)
+----------------------------------------------------------------------------------------
+
+testOpsBits :: forall w . 
+	(Ord w, Rep w, Bits w) => TestSeq -> String -> Gen w -> IO ()
+testOpsBits test tyName ws = do
+	testOpsNum test tyName ws
+
+	let ws2 = pair ws
+
+	sequence_
+	  [ testUniOp test (name ++ "/" ++ tyName) op lavaOp ws
+          | (name,op,lavaOp) <- 
+		[ ("complement",complement,complement)
+		]
+	  ]
+
+	
+pair :: (Applicative f) => f a -> f (a, a)
+pair ws = pure (,) <*> ws <*> ws
+
+triple :: (Applicative f) => f a -> f (a, a, a)
+triple ws = pure (,,) <*> ws <*> ws <*> ws
