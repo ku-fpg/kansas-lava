bea43fbadcb15cc4b42482b936f5a4a1cae17dc6
Andy Gill <andygill@ku.edu>
2010-10-11 23:29:02 -0500

    Adding new HandShake as a full Signal.

---
+++ Language/KansasLava/Handshake.hs
@@ -0,0 +1,100 @@
+{-# LANGUAGE TypeFamilies, ExistentialQuantification, FlexibleInstances, UndecidableInstances, FlexibleContexts,
+    ScopedTypeVariables, MultiParamTypeClasses, FunctionalDependencies,ParallelListComp  #-}
+
+module Language.KansasLava.Handshake where
+
+import Data.Word
+import Data.Int
+import Data.Bits
+import Data.List
+
+import Data.Reify
+import qualified Data.Traversable as T
+import Language.KansasLava.Types
+import Language.KansasLava.Signal
+
+import Language.KansasLava.Entity
+import Language.KansasLava.Entity.Utils
+import Language.KansasLava.Stream as S
+
+import Control.Applicative
+import Data.Sized.Unsigned as UNSIGNED
+import Data.Sized.Signed as SIGNED
+import Data.Sized.Sampled as SAMPLED
+import Data.Sized.Arith as Arith
+import qualified Data.Sized.Matrix as M
+import Data.Sized.Ix as X
+
+import Language.KansasLava.Comb
+import Data.List as List
+import Language.KansasLava.Wire
+import Language.KansasLava.Seq
+import Language.KansasLava.Protocols
+import Language.KansasLava.Shallow.FIFO 
+
+-----------------------------------------------------------------------------------------------
+
+-- Need to add concept of clock
+
+data Handshake a = Handshake { unHandshake :: Seq Bool -> Seq (Enabled a) }
+
+instance Signal Handshake where
+  liftS0 comb = Handshake $ \ _ -> enabledS $ liftS0 comb
+
+  liftS1 f (Handshake shake) = Handshake $ \ rd -> liftS1 (mapEnabled f) (shake rd)
+
+  liftS2 f (Handshake s1) (Handshake s2) = undefined
+	-- This is where the magic will happen, giving a token passing implementation for free
+
+  deepS _ = error "not possible to extract a Handshake without backedge"
+
+----------------------------------------------------------------------------------------------------
+
+toHandshake' :: (Rep a) => [Int] -> [Maybe a] -> Handshake a
+toHandshake' stutter xs = Handshake $ \ ready -> toSeq (fn stutter xs (fromSeq ready))
+	where
+	   -- We rely on the semantics of pattern matching to not match (x:xs)
+	   -- if (0:ps) does not match.
+	   fn (0:ps) (x:xs) c
+		    = x : case c of -- read c after issuing x
+			(Nothing:rs)         -> error "toVariableHandshake: bad protocol state (1)"
+			(Just True:rs)       -> fn ps xs rs          -- has been written
+			(Just False:rs)      -> fn (0:ps) (x:xs) rs -- not written yet
+	   fn (p:ps) xs c
+		    = Nothing : case c of
+			(Nothing:rs)         -> error "toVariableHandshake: bad protocol state (2)"
+			(Just _:rs) 	     -> fn (pred p:ps) xs rs -- nothing read
+
+fromHandshake' :: forall a . (Rep a) => [Int] -> Handshake a -> [Maybe a]
+fromHandshake' stutter (Handshake sink) = map snd internal
+   where
+	val :: Seq (Enabled a)
+	val = sink full
+
+	full :: Seq Bool
+	full = toSeq (map fst internal)
+
+	internal :: [(Bool,Maybe a)]
+	internal = fn stutter (fromSeq val)
+	
+	fn :: [Int] -> [Maybe (Enabled a)] -> [(Bool,Maybe a)]
+	fn (0:ps) ~(x:xs) = (True,rep) : rest
+	   where
+		(rep,rest) = case x of
+			       Nothing       -> error "fromVariableHandshake: bad reply to ready status"
+			       Just Nothing  -> (Nothing,fn (0:ps) xs)
+			       Just (Just v) -> (Just v,fn ps xs)
+	fn (p:ps) ~(x:xs) = (False,Nothing) : fn (pred p:ps) xs
+
+----------------------------------------------------------------------------------------------------
+
+
+fifoToHandshake :: (Rep a) => ShallowFIFO a -> IO (Handshake a)
+fifoToHandshake fifo = do
+	xs <- getFIFOContents fifo
+	return (toHandshake' (repeat 0) xs)
+
+handshakeToFifo :: (Rep a) => ShallowFIFO a -> Handshake a -> IO ()
+handshakeToFifo fifo sink = do
+	putFIFOContents fifo (fromHandshake' (repeat 0) sink)
+	return ()
