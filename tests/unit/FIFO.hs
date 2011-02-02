755fe750337f50d5c1ac537fd35a5ec941e99ea9
Andy Gill <andygill@ku.edu>
2011-02-02 16:33:58 -0600

    Improving the tests for FIFOs.

--- tests/unit/FIFO.hs
+++ tests/unit/FIFO.hs
@@ -12,25 +12,31 @@ import Data.Sized.Signed
 import Data.Sized.Arith
 import Data.Sized.Ix
 
+import Debug.Trace
+
+tests :: TestSeq -> IO ()
 tests test = do
-        
         -- testing FIFOs
 
-        let t str arb = testFIFO test str arb
+        let t str arb = testFIFO test str (dubSeq arb)
         t "U5"  (arbitrary :: Gen (Bool,Maybe U5)) (Witness :: Witness X1)
         t "U5"  (arbitrary :: Gen (Bool,Maybe U5)) (Witness :: Witness X2)
+        t "U5"  (arbitrary :: Gen (Bool,Maybe U5)) (Witness :: Witness X3)
         t "U5"  (arbitrary :: Gen (Bool,Maybe U5)) (Witness :: Witness X4)
+        t "U5"  (arbitrary :: Gen (Bool,Maybe U5)) (Witness :: Witness X5)
+        t "U5"  (arbitrary :: Gen (Bool,Maybe U5)) (Witness :: Witness X6)
+        t "U5"  (arbitrary :: Gen (Bool,Maybe U5)) (Witness :: Witness X7)
+        t "U5"  (arbitrary :: Gen (Bool,Maybe U5)) (Witness :: Witness X8)
 
 -- Need to fix memories first
 --        t "U1"  (arbitrary :: Gen (Bool,Maybe U1)) (Witness :: Witness X1)
 --        t "Bool"  (arbitrary :: Gen (Bool,Maybe Bool)) (Witness :: Witness X1)
 
 
--- stutters up to 16 cycles.
 testFIFO :: forall w sz sz1 . (Eq w, Rep w, Show w,
                                sz1 ~ ADD sz X1,
                                Size sz, Size sz1, 
-                               Rep sz, Rep sz1,
+                               Rep sz, Rep sz1,                 Num w,
                                Num sz, Num sz1)
         => TestSeq -> String -> Gen (Bool,Maybe w) -> Witness sz -> IO ()
 testFIFO (TestSeq test toList) tyName ws wit = do
@@ -38,15 +44,11 @@ testFIFO (TestSeq test toList) tyName ws wit = do
             vals    :: [Maybe w]
             (outBools,vals) = unzip $ toList ws
 
-        print $ take 20 $ outBools
-        print $ take 20 $ vals
-
-        let cir = (fifo wit low)  :: (Seq (Enabled w), Seq Bool)
-                                  -> (Seq Bool, Seq (Enabled w))
+        let cir = fifo wit low :: (Seq (Enabled w), Seq Bool) -> (Seq Bool, Seq (Enabled w))
         let thu :: Thunk (CSeq () (Bool,Enabled w))
             thu = Thunk cir
-                        (\ f -> let inp = toHandShaken vals back -- (observeRep "back" back)
-                                    (back,res) = cir (inp,toSeq outBools)
+                        (\ f -> let inp = toHandShaken vals back
+                                    (back,res) = f (inp,toSeq outBools)
                                  in pack (back,res)
                         )
 
@@ -55,6 +57,9 @@ testFIFO (TestSeq test toList) tyName ws wit = do
 
         let -- fifoSpec b c d | trace (show ("fifoSpec",take 10 b, take 10 c,d)) False = undefined
             fifoSpec :: [Maybe w] -> [Bool] -> [Maybe w] -> [Maybe w]
+--            fifoSpec _ _ state
+--                        | length [ () | Just _ <- state ] == fifoSize
+--                                && trace (show ("fifoLen",fifoSize)) False = undefined
             fifoSpec (val:vals) outs state
                         | length [ () | Just _ <- state ] < fifoSize
                         = fifoSpec2 vals  outs (val:state)
@@ -72,5 +77,6 @@ testFIFO (TestSeq test toList) tyName ws wit = do
 
         let res :: Seq (Bool,Enabled w)
             res = pack (undefinedS,toSeq $ fifoSpec vals (cycle outBools) [])
+        print (length vals)
         test ("fifo/sz_" ++ show fifoSize ++ "/" ++ tyName) (length vals) thu res
         return ()
