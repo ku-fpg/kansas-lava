5e51157c7012f29b67b7a6640da31cc632dabfd4
Garrin Kimmell <garrin.kimmell@gmail.com>
2011-02-24 11:35:18 -0600

    Cleanup of Radix module.

--- Language/KansasLava/Radix.hs
+++ Language/KansasLava/Radix.hs
@@ -1,3 +1,6 @@
+-- | Simple Radix trees, customized for Lava internals, for example is strict.
+-- It is used to represent function types and ROMS and RAMs.
+-- There is a requirement that all keys (list of bits) be the same length.
 module Language.KansasLava.Radix
 	( Radix		-- abstract
 	, empty
@@ -7,36 +10,30 @@ module Language.KansasLava.Radix
 
 import Prelude hiding (lookup)
 
--- | Simple Radix trees, customized for Lava internals, for example is strict.
--- It is used to represent function types and ROMS and RAMs.
--- There is a requirement that all keys (list of bits) be the same length.
 
 data Radix a
-  = Res !a
-  | NoRes
+  = Res !a -- ^ A value stored in the tree
+  | NoRes -- ^ Non-present value
+  -- | A split-node, left corresponds to 'True' key bit, right corresponds to 'False' key bit.
   | Choose !(Radix a) !(Radix a)
 	deriving Show
 
+-- | The empty tree
 empty :: Radix a
 empty = NoRes
 
+-- | Add a value (keyed by the list of bools) into a tree
 insert :: [Bool] -> a -> Radix a -> Radix a
-
 insert []    y (Res _) = Res $! y
 insert []    y NoRes   = Res $! y
 insert []    _ (Choose _ _) = error "inserting with short key"
+insert xs     y NoRes   = insert xs y (Choose NoRes NoRes)
+insert _  _ (Res _) = error "inserting with too long a key"
+insert (True:a) y (Choose l r) = Choose (insert a y l) r
+insert (False:a) y (Choose l r) = Choose l (insert a y r)
 
-insert (x:a) y NoRes   = insert (x:a) y expanded
-insert (_:_) _ (Res _) = error "inserting with too long a key"
-insert (x:a) y (Choose l r)
-	| x == True 	  = Choose (insert a y l) r
-	| x == False	  = Choose l (insert a y r)
-        | otherwise       = error "Unmatched pattern"
 
--- Would this be lifted?
-expanded :: Radix a
-expanded = Choose NoRes NoRes
-
+-- | Find a value in a radix tree
 lookup :: [Bool] -> Radix a -> Maybe a
 
 lookup [] (Res v) = Just v
