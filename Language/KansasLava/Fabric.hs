2bc7a91395803532a3f567c1a59edfd371f0a656
Andy Gill <andygill@ku.edu>
2011-03-06 11:37:32 -0600

    Adding commands to Fabric, including Vector support.

--- Language/KansasLava/Fabric.hs
+++ Language/KansasLava/Fabric.hs
@@ -3,10 +3,14 @@
 module Language.KansasLava.Fabric 
         ( Fabric(..)
         , Pad(..)
+        , runFabric
         , inStdLogic
+        , inStdLogicVector
         , inGeneric
         , outStdLogic
-        , fabric_example
+        , outStdLogicVector
+        , driving
+--        , fabric_example
         , backedges
         ) where
 
@@ -26,6 +30,12 @@ import Language.KansasLava.Utils
 data Pad = StdLogic_ (Seq Bool)
          | forall x . (Size x) => StdLogicVector_ (Seq (Unsigned x))
          | Generic_ Integer
+
+instance Show Pad where
+        show (StdLogic_ sq)       = "StdLogic_ " ++ show sq
+        show (StdLogicVector_ sq) = "StdLogicVector_ " ++ show sq
+        show (Generic_ i)         = "Generic_ " ++ show i
+
          -- TODO: the 2D Array
 
 -- The 'Fabric' structure, which is also a monad.
@@ -68,6 +78,12 @@ inGeneric nm = do
           Generic_ g -> return g
           _          -> fail "internal type error in inGeneric"
 
+inStdLogicVector :: (Size x) => String -> Fabric (Seq (Unsigned x))
+inStdLogicVector nm = do
+        pad <- input nm SL
+        case pad of
+          StdLogicVector_ sq -> return $ (unsigned) sq
+          _                  -> fail "internal type error in inStdLogic"
 
 output :: String -> Pad -> Fabric ()
 output nm pad = Fabric $ \ _ins -> ((),[],[(nm,pad)])
@@ -75,6 +91,45 @@ output nm pad = Fabric $ \ _ins -> ((),[],[(nm,pad)])
 outStdLogic :: String -> Seq Bool -> Fabric ()
 outStdLogic nm seq_bool = output nm (StdLogic_ seq_bool)
 
+outStdLogicVector :: (Size x) => String -> Seq (Unsigned x) -> Fabric ()
+outStdLogicVector nm sq = output nm (StdLogicVector_ sq)
+
+-------------------------------------------------------------------------------
+
+-- | 'driving' chains two fabrics together, leting
+-- the first one drive the second one. Note this
+-- is not the same as '(>>)', which makes no
+-- connections.
+
+-- NOTES: I'm unsure about the shadowing of names here.
+-- It will work, as long as inputs and output never
+-- interset.
+
+infixr 5 `driving`
+
+driving :: Fabric a -> Fabric b -> Fabric b
+driving (Fabric f) (Fabric g) = Fabric $ \ ins ->
+    let (_,f_in_names,f_outs) = f ins
+        (b,g_in_names,g_outs) = g (f_outs ++ ins)
+    in ( b
+       , f_in_names ++ [ (nm,ty) 
+                       | (nm,ty) <- g_in_names
+                       , nm `notElem` map fst f_outs ]
+       , [ (nm,ty) 
+         | (nm,ty) <- f_outs
+         , nm `notElem` map fst g_in_names
+         ] ++ g_outs
+       )
+
+
+-- 'runFabric'  runs a Fabric () with arguments, and gives a (structured) reply.
+runFabric :: Fabric () -> [(String,Pad)] -> [(String,Pad)]
+runFabric (Fabric f) args = result 
+        where ((),_arg_types,result) = f args
+
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+{-
 fabric_example :: Fabric ()
 fabric_example = do
         i0 <- inStdLogic "i0"
@@ -87,7 +142,7 @@ fabric_example = do
           halfAdder a b = (carry,sum_)
                 where carry = and2 a b
                       sum_  = xor2 a b
-
+-}
 -------------------------------------------------------------------------------
 
 backedges :: (MonadFix m) => (b -> m (a,b)) -> m a
