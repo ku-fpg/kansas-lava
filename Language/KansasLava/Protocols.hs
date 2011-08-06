0e6c9e2207f805c5df118bc5eba805d16f48efcc
Andrew Gill <andy@orange.local>
2011-08-06 00:18:00 -0500

    Adding fluxCapacitor.

--- Language/KansasLava/Protocols.hs
+++ Language/KansasLava/Protocols.hs
@@ -18,7 +18,8 @@ module Language.KansasLava.Protocols (
 	, mapStatus
 	, noStatus
 	, stack
-	
+	-- for now
+	, upFlux, downFlux, fluxCapacitor
 	) where
 
 import Language.KansasLava.Protocols.Enabled
@@ -33,8 +34,8 @@ import Language.KansasLava.Utils
 import Language.KansasLava.Seq
 import Language.KansasLava.Signal
 
---import Language.KansasLava.Stream (Stream(..))
---import qualified Language.KansasLava.Stream as Stream
+import Language.KansasLava.Stream (Stream(..))
+import qualified Language.KansasLava.Stream as Stream
 
 ---------------------------------------------------------------------------
 
@@ -221,3 +222,83 @@ liftHandShake1 fn ~(en_a,ack) = (ready,(),en_b)
 
 
 -}
+
+upFlux :: forall a c1 sig1 c2 sig2 . (Rep a, Clock c1, Clock c2, sig1 ~ CSeq c1, sig2 ~ CSeq c2)
+       => ( sig2 Bool, sig1 a ) -> sig2 (Enabled a)
+upFlux ~( ~(Seq s_b d_b) , ~(Seq s_a d_a)) = res
+  where
+	res = Seq s_enB d_enB
+
+	s_enB = upsample0 s_a s_b
+	d_enB = D $ Port "o0" $ E $
+			Entity (Prim "pair") 
+				[("o0",bitTypeOf res)]
+				[("i0",B,Port "o_en" $ E $ e)
+				,("i1",ty,Port "o0" $ E $ e)
+				]
+	
+	ty = bitTypeOf (undefined :: Seq a)
+
+	e = Entity (External "upflux")
+                   [("o_en",B)
+                   ,("o0",ty)
+		   ,("o_clk_en",B)
+		   ]
+                   [("i0",ty, unD d_a)
+                   ,("go",B, unD d_b)
+                   ]
+
+	-- first value is unknown, because this is a clock enable
+        upsample0 ss readys = 
+		XMaybe (pureX False, unknownX) `Cons` upsample ss readys
+
+	-- Only steps when you have a Boolean yes
+	upsample :: Rep a => Stream (X a) -> Stream (X Bool) -> Stream (X (Maybe a))
+        upsample ss (XBool ((WireVal True)) `Cons` readys) 
+		= case ss of
+		   (s `Cons` ss') -> XMaybe (pureX True, s) `Cons` upsample ss' readys
+        upsample ss (XBool ((WireVal False)) `Cons` readys) 
+		= XMaybe (pureX False, unknownX) `Cons` upsample ss readys
+        upsample _ (XBool _ `Cons` _) = Stream.repeat unknownX
+
+downFlux :: forall a c1 sig1 c2 sig2 . (Rep a, Clock c1, Clock c2, sig1 ~ CSeq c1, sig2 ~ CSeq c2)
+       => sig1 (Enabled a) -> ( sig1 Bool , sig2 a )
+downFlux sig = (Seq s_out_b d_out_b, Seq s_out_a d_out_a )
+   where
+	(Seq s_in_b d_in_b,Seq s_in_a d_in_a) = unpack sig
+	
+	ty = bitTypeOf (undefined :: Seq a)
+
+
+	s_out_b = s_in_b	-- pass through for shallow
+	d_out_b = D $ Port "go" $ E $ e
+	
+	s_out_a = downsample s_in_b s_in_a
+	d_out_a = D $ Port "o0" $ E $ e
+	
+	e = Entity (External "downflux")
+                   [("go",B)
+                   ,("o0",ty)
+		   ]
+                   [("i0",ty, unD d_in_a)
+                   ,("en",B, unD d_in_b)
+                   ]
+
+	downsample :: Rep a => Stream (X Bool) -> Stream (X a) -> Stream (X a)
+	downsample (XBool (WireVal True) `Cons` ens) (s `Cons` ss) 
+		= s `Cons` downsample ens ss
+	downsample (XBool (WireVal False) `Cons` ens) (_ `Cons` ss) 
+		= downsample ens ss
+	downsample (XBool _ `Cons` _) _
+		= Stream.repeat unknownX
+		
+
+fluxCapacitor :: forall a b c sig . (Rep a, Rep b, Clock c, sig ~ CSeq c)
+	=> (forall c' sig' . (Clock c', sig' ~ CSeq c') => sig' a -> sig' b)
+	-> sig (Enabled a) -> sig (Enabled b)
+fluxCapacitor f sig = upFlux ( clk_en, f slow_a )
+    where
+	clk_en :: sig Bool
+	slow_a :: Seq a	-- can be *any*, so we use the standard clock for wiring.
+	( clk_en, slow_a ) = downFlux sig
+
