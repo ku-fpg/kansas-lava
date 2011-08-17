ae895647bc8fcc26cdad204e57aca83cb02ddac3
Andrew Gill <andy@dhcp248.ittc.ku.edu>
2011-08-17 16:52:08 -0500

    Reorder fifo1 and fifo2

--- Language/KansasLava/Protocols/Patch.hs
+++ Language/KansasLava/Protocols/Patch.hs
@@ -459,6 +459,26 @@ matrixMuxPatch = matrixMuxPatch' $$ readyToAckBridge where
 
 -- (There are larger FIFO's in the Kansas Lava Cores package.)
 
+fifo1 :: forall c sig a . (Clock c, sig ~ CSeq c, Rep a) 
+      => Patch (sig (Enabled a)) 	(sig (Enabled a))
+	       (sig Ack)		(sig Ack)
+fifo1 ~(inp,ack) = (toAck have_read, out)
+   where
+	have_read = (state .==. 0) .&&. isEnabled inp
+	written   = (state .==. 1) .&&. fromAck ack
+
+	state :: sig X2
+	state = register 0
+	      $ cASE [ (have_read,	pureS 1)
+		     , (written,	pureS 0)
+		     ] state
+
+	store :: sig a
+	store = cASE [ (have_read,enabledVal inp)
+		     ]
+	      $ delay store
+
+	out = packEnabled (state .==. 1) store
 
 fifo2 :: forall c sig a . (Clock c, sig ~ CSeq c, Rep a) 
     => Patch (sig (Enabled a)) 	 (sig (Enabled a))
@@ -506,26 +526,6 @@ fifo2 = ackToReadyBridge $$ fifo2' where
 
         out = packEnabled (state ./=. 0) outval
 
-fifo1 :: forall c sig a . (Clock c, sig ~ CSeq c, Rep a) 
-      => Patch (sig (Enabled a)) 	(sig (Enabled a))
-	       (sig Ack)		(sig Ack)
-fifo1 ~(inp,ack) = (toAck have_read, out)
-   where
-	have_read = (state .==. 0) .&&. isEnabled inp
-	written   = (state .==. 1) .&&. fromAck ack
-
-	state :: sig X2
-	state = register 0
-	      $ cASE [ (have_read,	pureS 1)
-		     , (written,	pureS 0)
-		     ] state
-
-	store :: sig a
-	store = cASE [ (have_read,enabledVal inp)
-		     ]
-	      $ delay store
-
-	out = packEnabled (state .==. 1) store
 
 ---------------------------------------------------------------------------------
 -- Retiming
