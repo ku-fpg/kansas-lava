b8560f6f0f88f4398c5f759146fa724579339481
Andy Gill <andygill@ku.edu>
2011-10-18 22:30:33 -0500

    Printing of sequences are separated by bars.

--- Language/KansasLava/Signal.hs
+++ Language/KansasLava/Signal.hs
@@ -126,7 +126,7 @@ primS3 f nm = primXS3 (\ a b c -> optX $ liftM3 f (unX a) (unX b) (unX c)) nm
 instance (Rep a, Show a) => Show (Signal c a) where
 	show (Signal vs _) = show' vs
 	  where
-	     show' (Cons a opt_as) = showRep a ++ " " ++ maybe "" show' opt_as
+	     show' (Cons a opt_as) = showRep a ++ maybe "" (\ as -> " | " ++ show' as) opt_as
 
 instance (Rep a, Eq a) => Eq (Signal c a) where
 	-- Silly question; never True; can be False.
