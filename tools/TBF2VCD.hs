be66349733560dbc69198438ea0b89cb6c8d1291
Andy Gill <andygill@ku.edu>
2011-11-05 16:39:35 -0500

    Changing the tbf2vcd tool.

--- tools/TBF2VCD.hs
+++ tools/TBF2VCD.hs
@@ -11,22 +11,22 @@ main = do
 	main2 cmds
 
 main2 :: [String] -> IO ()
-main2 ["--diff",sig,inbits,outbits] = main4 sig inbits outbits
-main2 ["--clock",clk,sig,bits'] | all C.isDigit clk
-	= main3 True (read clk) sig bits'
-main2 [clk,sig,bits'] | all C.isDigit clk
-	= main3 False (read clk) sig bits'
-main2 _ = error $ "usage:\n   tbf2vcd [--clock] (clockrate-in-ns) <.sig-file> <.bits-file>\n" ++
-                          "   tbf2vcd --diff <.sig-file> <.left-bits-file> <.right-bits-file>"
+main2 ["--diff",sig,inbits,outbits,vcd_out] = main4 sig inbits outbits vcd_out
+main2 ["--clock",clk,sig,bits',vcd_out] | all C.isDigit clk
+	= main3 True (read clk) sig bits' vcd_out
+main2 [clk,sig,bits',vcd_out] | all C.isDigit clk
+	= main3 False (read clk) sig bits' vcd_out
+main2 _ = error $ "usage:\n   tbf2vcd [--clock] (clockrate-in-ns) <.sig-file> <.bits-file> <vcd-file>\n" ++
+                          "   tbf2vcd --diff <.sig-file> <.left-bits-file> <.right-bits-file> <vcd-file>"
 
-main3 :: Bool -> Integer -> FilePath -> FilePath -> IO ()
-main3 ifClk clkRate sigName bitsName = do
+main3 :: Bool -> Integer -> FilePath -> FilePath ->  FilePath -> IO ()
+main3 ifClk clkRate sigName bitsName vcdFile = do
 	sig <- read <$> readFile sigName
 	str <- lines <$> readFile bitsName
-	putStrLn $ toVCDFile ifClk clkRate $ readTBF str sig
+	writeVCDFile ifClk clkRate vcdFile $ readTBF str sig
 
-main4 :: FilePath -> FilePath -> FilePath -> IO ()
-main4 sigfile leftfile rightfile = do
+main4 :: FilePath -> FilePath -> FilePath ->  FilePath -> IO ()
+main4 sigfile leftfile rightfile vcdFile = do
     left  <- lines <$> readFile leftfile
     right <- lines <$> readFile rightfile
     sig   <- read  <$> readFile sigfile
@@ -34,10 +34,10 @@ main4 sigfile leftfile rightfile = do
     let t1 = readTBF left  sig
         t2 = readTBF right sig
 
-    writeFile "diff.vcd" $ vcdDiff t1 t2
+    vcdDiff t1 t2 vcdFile
 
-vcdDiff :: VCD -> VCD -> String
-vcdDiff (VCD m1) (VCD m2) = toVCDFile False 10 t
+vcdDiff :: VCD -> VCD -> FilePath -> IO ()
+vcdDiff (VCD m1) (VCD m2) filePath = writeVCDFile False 10 filePath t
     where t = VCD $ [ ("trace1_" ++ k,v) | (k,v) <- m1 ]
                     ++
                     [ ("trace2_" ++ k,v) | (k,v) <- m2 ]
