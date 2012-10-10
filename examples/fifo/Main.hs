c69e3484e53c0f5c6e6e2fb5a860ac256ae5939a
Andy Gill <andygill@ku.edu>
2012-10-10 15:57:30 -0500

    First cut at new monad driver. No clock yet.

--- examples/fifo/Main.hs
+++ examples/fifo/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE ScopedTypeVariables, RecursiveDo #-}
+{-# LANGUAGE ScopedTypeVariables, RecursiveDo, KindSignatures, RankNTypes, GADTs #-}
 module Main where
 
 import Language.KansasLava
@@ -16,6 +16,15 @@ import Data.Sized.Unsigned
 import Control.Concurrent.STM
 import Control.Concurrent
 import System.Environment
+
+import Control.Concurrent.MVar
+import System.IO.Unsafe
+import Control.Monad.Trans.Class
+import System.Random
+import Control.Monad
+import Control.Monad.Trans.Trace
+
+
 
 main :: IO ()
 main = do
@@ -86,12 +95,16 @@ main2 ["driver"] = do
 
         v0 <- atomically $ newEmptyTMVar
         v1 <- atomically $ newEmptyTMVar
+        clk <- atomically $ newEmptyTMVar
 
         let en :: Seq Bool
             en = toS (cycle (take 10 (repeat False) ++ take 10 (repeat True)))
 
         let proto_driver :: SuperFabric IO ()
             proto_driver = do
+
+--               clk <-
+
                rec i_ready :: Seq Ready <- inStdLogic "i_ready"
                    val_i :: Seq (Enabled U8) <- liftIO $ txProtocolS v0 en i_ready
                    outStdLogicVector "i0"      (enabledVal val_i :: Seq U8)
@@ -116,10 +129,56 @@ main2 ["driver"] = do
 
         runFabricWithDriver (observeFabric probeS dut_proxy) proto_driver
 
-        let loop0 n = do
-                print ("loop0",n)
-                atomically $ putTMVar v0 (fromIntegral (n :: Integer) :: U8)
-                if n > 100000 then return () else loop0 (n+1)
+        std0 <- getStdGen
+        var <- newMVar std0
+        env <- return $ Env
+          { env_rand = do
+                std <- takeMVar var
+                let (n,std') = random std
+                putMVar var std'
+                return n
+          , env_randR = \ (a,b) -> do
+                std <- takeMVar var
+                let (n,std') = randomR (a,b) std
+                putMVar var std'
+                return n
+          , in_val = v0
+          , the_clk = clk
+          }
+
+{-        let stuff = do
+                i <- randR (0::Int,10)
+                if i == 0 then reset
+                          else loop
+
+            loop = do
+                w <- randR (0::Int,100)
+                wait w
+                d <- randR (0::Int,255)
+                b <- send d
+                if b then stuff
+                     else loop
+
+        let interleave ~(FifoM f) = FifoM $ unsafeInterleaveIO . f
+        let FifoM f = interp1 interleave stuff
+        a <- f env
+--        print a
+        print a
+        return ()
+-}
+
+
+
+        let loop0 :: Integer -> TraceT FifoE FifoM ()
+            loop0 n = do
+                liftIO $ print ("loop0",n)
+--                n <- r
+                w <- randR (0::Int,10)
+                wait w
+                r <- randR (0::Int,255)
+                send r
+--                atomically $ putTMVar v0 (fromIntegral (n :: Integer) :: U8)
+                if n > 10 then return () else loop0 (n+1)
 
         let loop1 = do
                 v :: U8 <- atomically $ takeTMVar v1
@@ -128,9 +187,68 @@ main2 ["driver"] = do
                 print ("loop1",v)
                 loop1
 
-        forkIO $ loop0 0
+        let interleave ~(FifoM f) = FifoM $ unsafeInterleaveIO . f
+
+        forkIO $ do
+                let FifoM f = interp1 interleave (loop0 0) :: FifoM (Trace FifoE ())
+                a <- f env
+                print a
 
         loop1
+
+------------------------------------------------------------------
+
+data FifoE :: * where
+        SendEvent :: Maybe (U8,Integer) -> FifoE    -- val x cycle-sent
+        WaitEvent :: Int -> FifoE
+        ResetEvent :: FifoE
+
+instance Show FifoE where
+        show (SendEvent v) = "send(" ++ show v ++ ")"
+        show (WaitEvent n)  = "pause(" ++ show n ++ ")"
+        show (ResetEvent)  = "reset"
+
+data FifoM :: * -> * where
+        FifoM :: (Env -> IO a)          -> FifoM a
+
+instance Monad FifoM where
+        return a = FifoM $ \ env -> return a
+        (FifoM m) >>= k = FifoM $ \ env -> do
+                r <- m env
+                case k r of
+                  FifoM m -> m env
+
+instance MonadIO FifoM where
+        liftIO m = FifoM $ \ _ -> m
+
+data Env = Env
+        { env_rand  :: forall r . (Random r) => IO r -- a random number generator
+        , env_randR :: forall r . (Random r) => (r,r) -> IO r
+        , in_val :: TMVar U8
+        , the_clk :: TMVar Integer
+        }
+
+
+send :: Int -> TraceT FifoE FifoM Bool
+send n = event $ FifoM $ \ env -> do
+        let dat = fromIntegral n :: U8
+        -- waits until sent
+        atomically $ putTMVar (in_val env) dat
+        return (SendEvent (Just (dat,0)), True)
+
+wait :: Int -> TraceT FifoE FifoM ()
+wait n = event $ FifoM $ \ env -> do { return (WaitEvent n,()) }
+
+reset :: TraceT FifoE FifoM ()
+reset = event $ FifoM $ \ env -> do { return (ResetEvent,()) }
+
+rand :: (Random r) => TraceT FifoE FifoM r
+rand = lift $ FifoM $ \ env -> do { r <- env_rand env ; return r }
+
+randR :: (Random r) => (r,r) -> TraceT FifoE FifoM r
+randR (a,b) = lift $ FifoM $ \ env -> do { r <- env_randR env (a,b) ; return r }
+
+------------------------------------------------------------------
 
 
 
