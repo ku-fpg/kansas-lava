a581194497ce7246ab7b0faf4261aabf432d5831
Andy Gill <andygill@ku.edu>
2013-02-07 11:22:48 -0600

    Removing AckBox, ReadBox and Patch.

--- Language/KansasLava/Protocols.hs
+++ Language/KansasLava/Protocols.hs
@@ -2,19 +2,13 @@
 module Language.KansasLava.Protocols (
 	module Language.KansasLava.Protocols.Enabled,
 	module Language.KansasLava.Protocols.Memory,
-	module Language.KansasLava.Protocols.AckBox,
-	module Language.KansasLava.Protocols.ReadyBox,
 	module Language.KansasLava.Protocols.Types,
-	module Language.KansasLava.Protocols.Patch,
 	Response(..)
 	) where
 
 import Language.KansasLava.Protocols.Enabled
 import Language.KansasLava.Protocols.Memory
-import Language.KansasLava.Protocols.AckBox
-import Language.KansasLava.Protocols.ReadyBox
 import Language.KansasLava.Protocols.Types
-import Language.KansasLava.Protocols.Patch
 
 ----
 import Language.KansasLava.Types
